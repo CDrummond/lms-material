b0e5192f36a997b08066670ebe0031f771bfaf05
Craig Drummond <craig.p.drummond@gmail.com>
2018-10-12 17:00:01 +0100

    Add basic sync support

---
+++ MaterialSkin/HTML/material/js/sync-dialog.js
@@ -0,0 +1,59 @@
+
+Vue.component('lms-sync-dialog', {
+    template: `
+      <v-dialog v-model="show" width="450" class="lms-dialog">
+        <v-card>
+          <v-card-text>
+            <v-container grid-list-md>
+              <v-layout wrap>
+                <v-flex xs12>Select which other player you would like to synchronise playback with.</v-flex>
+                <v-flex xs12>
+                  <v-select :items="players" label="Synchronise with" v-model="sync" item-text="label" item-value="key"></v-select>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn flat @click.native="close()">Close</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+`,
+    props: [],
+    data() {
+        return {
+            show: false,
+            sync: undefined,
+            players: []
+        }
+    },
+    mounted() {
+        bus.$on('synchronise', function() {
+            this.playerId = this.$store.state.player.id;
+            lmsCommand(this.playerId, ["sync", "?"]).then(({data}) => {
+                if (data && data.result && undefined!=data.result._sync) {
+                    this.sync=this.syncOrig=data.result._sync;
+                    this.players=[];
+                    this.players.push({key:"-", label:"Do not sync"});
+                    this.$store.state.players.forEach(i => {
+                        if (i.id!==this.playerId) {
+                            this.players.push({key:i.id, label:i.name});
+                        }
+                    });
+                    this.show = true;
+                }
+            });
+        }.bind(this));
+    },
+    methods: {
+        close() {
+            this.show=false;
+            if (this.sync!==this.syncOrig) {
+            console.log("update");
+                lmsCommand(this.playerId, ["sync", this.sync]);
+            }
+        }
+    }
+})
+

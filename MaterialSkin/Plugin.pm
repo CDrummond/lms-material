09f8e121afc7c7416f9b838e2e037e48c1d73147
Craig Drummond <craig.p.drummond@gmail.com>
2018-10-18 17:08:59 +0100

    Add JSON files in main and lang folders

--- MaterialSkin/Plugin.pm
+++ MaterialSkin/Plugin.pm
@@ -3,30 +3,27 @@ package Plugins::MaterialSkin::Plugin;
 use File::Basename;
 use File::Spec::Functions qw(catfile catdir);
 
-use constant FONT_DIR => 'HTML/material/font';
-
 # this plugin registers the helper files (fonts, manifest) as raw downloads
 # this will allow Logitech Media Server to serve those files without a patch
 sub initPlugin {
-	my $baseDir = dirname($INC{'Plugins/MaterialSkin/Plugin.pm'});
-	my $fontDir = catdir($baseDir, FONT_DIR);
+    my $baseDir = dirname($INC{'Plugins/MaterialSkin/Plugin.pm'});
+    my @dirs = ('HTML/material/', 'HTML/material/font', 'HTML/material/lang');
 
-	opendir(DIR, $fontDir) || do {
-		Slim::Utils::Log::logError('MaterialSkin: failed to read base folder with fonts');
-		return;
-	};
+    foreach $d (@dirs) {
+        my $path = catdir($baseDir, $d);
+        if opendir(dir, $path) {
+            my @entries = readdir(dir);
+            close(dir);
 
-	my @entries = readdir(DIR);
-
-	close(DIR);
-
-	for my $file (@entries) {
-		# extend the list of file extensions if needed
-		if ($file =~ /\.(?:eot|svg|woff2?|ttf|json)$/) {
-			$file = catfile($fontDir, $file);
-			Slim::Web::Pages->addRawDownload(basename($file), $file, Slim::Music::Info::typeFromSuffix($file));
-		}
-	}
+            for my $file (@entries) {
+                # extend the list of file extensions if needed
+                if ($file =~ /\.(?:eot|svg|woff2?|ttf|json)$/) {
+                    $file = catfile($path, $file);
+                    Slim::Web::Pages->addRawDownload(basename($file), $file, Slim::Music::Info::typeFromSuffix($file));
+                }
+            }
+        }
+    }
 }
 
 1;

ad06d7bd7bd4c986ea4c5c11fdbad09d4e14e10e
Craig Drummond <craig.p.drummond@gmail.com>
2018-10-18 17:09:57 +0100

    Also handle empty translations

--- MaterialSkin/HTML/material/js/i18n.js
+++ MaterialSkin/HTML/material/js/i18n.js
@@ -12,7 +12,7 @@ function setTranslation(trans) {
 }
 
 function i18n(str, val) {
-    if (undefined==translation || !(str in translation)) {
+    if (undefined==translation || !(str in translation) || translation[str]==="") {
         return undefined==val ? str : str.replace("%1", val);
     }
     return undefined==val ? translation[str] : translation[str].replace("%1", val);
@@ -20,12 +20,12 @@ function i18n(str, val) {
 
 function i18np(singular, plural, val) {
     if (1==val) {
-        if (undefined==translation || !(singular in translation)) {
+        if (undefined==translation || !(singular in translation) || translation[singular]==="") {
             return singular.replace("%1", val);
         }
         return translation[singular].replace("%1", val);
     } else {
-        if (undefined==translation || !(plural in translation)) {
+        if (undefined==translation || !(plural in translation) || translation[plural]==="") {
             return plural.replace("%1", val);
         }
         return translation[plural].replace("%1", val);

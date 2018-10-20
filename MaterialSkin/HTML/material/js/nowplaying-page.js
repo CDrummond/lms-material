723a5ee5a18c5863b5f008f1ad10319de7a64d5e
Craig Drummond <craig.p.drummond@gmail.com>
2018-10-20 21:03:10 +0100

    Lyrics and artist info

--- MaterialSkin/HTML/material/js/nowplaying-page.js
+++ MaterialSkin/HTML/material/js/nowplaying-page.js
@@ -8,43 +8,72 @@
 var lmsNowPlaying = Vue.component("LmsNowPlaying", {
     template: `
       <div class="np-page" v-if="playerStatus.isOn">
-        <p class="np-text ellipsis" v-if="playerStatus.current.title">{{playerStatus.current.title}}</p>
-        <p class="np-text" v-else>&nbsp;</p>
-        <p class="np-subtext ellipsis" v-if="playerStatus.current.artist">{{playerStatus.current.artist}}</p>
-        <p class="np-subtext" v-else>&nbsp;</p>
-        <p class="np-subtext ellipsis" v-if="playerStatus.current.album">{{playerStatus.current.album}}</p>
-        <p class="np-subtext" v-else>&nbsp;</p>
-        <img :src="cover" class="np-image"></img>
+        <div v-if="info.show" class="np-info">
+          <v-tabs centered v-model="info.tab">
+            <template v-for="(tab, index) in info.tabs">
+              <v-tab :key="index">{{tab.title}}</v-tab>
+              <v-tab-item :key="index">
+                <v-card flat>
+                  <v-card-text class="np-info-text" v-html="tab.text"></v-card-text>
+                </v-card>
+              </v-tab-item>
+            </template>
+          </v-tabs>
+          <v-card>
+            <v-divider></v-divider>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn flat @click="info.show = false">{{trans.close}}</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </div>
+        <div v-else>
+          <p class="np-text ellipsis" v-if="playerStatus.current.title">{{playerStatus.current.title}}</p>
+          <p class="np-text" v-else>&nbsp;</p>
+          <p class="np-subtext ellipsis" v-if="playerStatus.current.artist">{{playerStatus.current.artist}}</p>
+          <p class="np-subtext" v-else>&nbsp;</p>
+          <p class="np-subtext ellipsis" v-if="playerStatus.current.album">{{playerStatus.current.album}}</p>
+          <p class="np-subtext" v-else>&nbsp;</p>
+          <img v-if="!info.show" :src="cover" class="np-image" @contextmenu="showMenu"></img>
+          <v-menu v-model="menu.show" :position-x="menu.x" :position-y="menu.y" absolute offset-y>
+            <v-list>
+              <v-list-tile>
+                <v-list-tile-title @click="info.show=true">{{menu.text}}</v-list-tile-title>
+              </v-list-tile>
+            </v-list>
+          </v-menu>
 
-        <v-layout text-xs-center row wrap class="np-controls">
-          <v-flex xs6 class="np-pos">{{playerStatus.current.time | displayTime}}</v-flex>
-          <v-flex xs6 class="np-duration">{{playerStatus.current.duration | displayTime}}</v-flex>
-          <v-flex xs12><v-slider id="pos-slider" class="np-slider" :value='playerStatus.current.time' :max='playerStatus.current.duration' @click.native="sliderChanged($event)"></v-slider></v-flex>
-          <v-flex xs4>
-            <v-layout text-xs-center>
-              <v-flex xs6>
-               <v-btn flat icon v-if="playerStatus.playlist.repeat===1" @click="doAction(['playlist', 'repeat', 0])"><v-icon>repeat_one</v-icon></v-btn>
-               <v-btn flat icon v-else-if="playerStatus.playlist.repeat===2" @click="doAction(['playlist', 'repeat', 1])"><v-icon>repeat</v-icon></v-btn>
-               <v-btn flat icon v-else @click="doAction(['playlist', 'repeat', 2])" class="dimmed-icon"><v-icon>repeat</v-icon></v-btn>
-              </v-flex>
-              <v-flex xs6><v-btn flat icon @click="doAction(['button', 'jump_rew'])"><v-icon large>skip_previous</v-icon></v-btn></v-flex>
-            </v-layout>
-          </v-flex>
-          <v-flex xs4>
-            <v-btn flat icon large v-if="playerStatus.isPlaying" @click="doAction(['pause'])"><v-icon x-large>pause_circle_outline</v-icon></v-btn>
-            <v-btn flat icon large v-else @click="doAction(['play'])"><v-icon x-large>play_circle_outline</v-icon></v-btn>
-          </v-flex>          
-          <v-flex xs4>
-            <v-layout text-xs-center>
-              <v-flex xs6><v-btn flat icon @click="doAction(['playlist', 'index', '+1'])"><v-icon large>skip_next</v-icon></v-btn></v-flex>
-              <v-flex xs6>
-                <v-btn flat icon v-if="playerStatus.playlist.shuffle===2" @click="doAction(['playlist', 'shuffle', 0])"><v-icon class="shuffle-albums">shuffle</v-icon></v-btn>
-                <v-btn flat icon v-else-if="playerStatus.playlist.shuffle===1" @click="doAction(['playlist', 'shuffle', 2])"><v-icon>shuffle</v-icon></v-btn>
-                <v-btn flat icon v-else @click="doAction(['playlist', 'shuffle', 1])" class="dimmed-icon"><v-icon>shuffle</v-icon></v-btn>
-              </v-flex>
-            </v-layout>
-          </v-flex>
-        </v-layout>
+          <v-layout text-xs-center row wrap class="np-controls">
+            <v-flex xs6 class="np-pos" v-if="!info.show">{{playerStatus.current.time | displayTime}}</v-flex>
+            <v-flex xs6 class="np-duration" v-if="!info.show">{{playerStatus.current.duration | displayTime}}</v-flex>
+            <v-flex xs12 v-if="!info.show"><v-slider id="pos-slider" class="np-slider" :value='playerStatus.current.time' :max='playerStatus.current.duration' @click.native="sliderChanged($event)"></v-slider></v-flex>
+            <v-flex xs4>
+              <v-layout text-xs-center>
+                <v-flex xs6>
+                 <v-btn flat icon v-if="playerStatus.playlist.repeat===1" @click="doAction(['playlist', 'repeat', 0])"><v-icon>repeat_one</v-icon></v-btn>
+                 <v-btn flat icon v-else-if="playerStatus.playlist.repeat===2" @click="doAction(['playlist', 'repeat', 1])"><v-icon>repeat</v-icon></v-btn>
+                 <v-btn flat icon v-else @click="doAction(['playlist', 'repeat', 2])" class="dimmed-icon"><v-icon>repeat</v-icon></v-btn>
+                </v-flex>
+                <v-flex xs6><v-btn flat icon @click="doAction(['button', 'jump_rew'])"><v-icon large>skip_previous</v-icon></v-btn></v-flex>
+              </v-layout>
+            </v-flex>
+            <v-flex xs4>
+              <v-btn flat icon large v-if="playerStatus.isPlaying" @click="doAction(['pause'])"><v-icon x-large>pause_circle_outline</v-icon></v-btn>
+              <v-btn flat icon large v-else @click="doAction(['play'])"><v-icon x-large>play_circle_outline</v-icon></v-btn>
+            </v-flex>
+            <v-flex xs4>
+              <v-layout text-xs-center>
+                <v-flex xs6><v-btn flat icon @click="doAction(['playlist', 'index', '+1'])"><v-icon large>skip_next</v-icon></v-btn></v-flex>
+                <v-flex xs6>
+                  <v-btn flat icon v-if="playerStatus.playlist.shuffle===2" @click="doAction(['playlist', 'shuffle', 0])"><v-icon class="shuffle-albums">shuffle</v-icon></v-btn>
+                  <v-btn flat icon v-else-if="playerStatus.playlist.shuffle===1" @click="doAction(['playlist', 'shuffle', 2])"><v-icon>shuffle</v-icon></v-btn>
+                  <v-btn flat icon v-else @click="doAction(['playlist', 'shuffle', 1])" class="dimmed-icon"><v-icon>shuffle</v-icon></v-btn>
+                </v-flex>
+              </v-layout>
+            </v-flex>
+          </v-layout>
+        </v-div>
       </div>
 `,
     props: [],
@@ -56,10 +85,21 @@ var lmsNowPlaying = Vue.component("LmsNowPlaying", {
                     isPlaying: 1,
                     current: { canseek:1, artwork_url:undefined, coverid: undefined, duration:0, time:0, title:undefined, artist:undefined, album:undefined },
                     playlist: { shuffle:0, repeat: 0 },
-                 }
+                 },
+                 info: { installed: false, show: false, tab:0,
+                         tabs: [ { title:undefined, text: "Wibble" }, { title:undefined, text: "Wobble" }, { title:undefined, text: "abc" } ] },
+                 menu: { show: false, x:0, y:0, text: undefined },
+                 trans: { close: undefined }
                 };
     },
     mounted() {
+        // Music and Artist info plugin installled?
+        lmsCommand("", ["musicartistinfo", "localfiles", 0, 0]).then(({data}) => {
+            if (data && data.result && data.result.window) {
+                this.info.installed = true;
+            }
+        });
+
         bus.$on('playerStatus', function(playerStatus) {
             // Has cover changed?
             if (playerStatus.current.artwork_url!=this.playerStatus.current.artwork_url ||
@@ -115,8 +155,31 @@ var lmsNowPlaying = Vue.component("LmsNowPlaying", {
         }.bind(this));
         // Refresh status now, in case we were mounted after initial status call
         bus.$emit('refreshStatus');
+
+        bus.$on('langChanged', function() {
+            this.initItems();
+        }.bind(this));
+        this.initItems();
     },
     methods: {
+        initItems() {
+            this.trans.close=i18n("Close");
+            this.menu.text=i18n("Show information");
+            this.info.tabs[0].title=i18n("Lyrics");
+            this.info.tabs[1].title=i18n("Artist Biography");
+            this.info.tabs[2].title=i18n("Album Review");
+        },
+        showMenu(event) {
+            event.preventDefault();
+            if (this.info.installed && this.playerStatus.current.title && this.playerStatus.current.artist) {
+                this.menu.show = false;
+                this.menu.x = event.clientX;
+                this.menu.y = event.clientY;
+                this.$nextTick(() => {
+                    this.menu.show = true;
+                });
+            }
+        },
         doAction(command) {
             bus.$emit('playerCommand', command);
         },
@@ -127,6 +190,43 @@ var lmsNowPlaying = Vue.component("LmsNowPlaying", {
                 const width = rect.width;
                 this.doAction(['time', Math.floor(this.playerStatus.current.duration * pos / rect.width)]);
             }
+        },
+        fetchLyrics() {
+            if (this.info.tabs[0].songartist!=this.playerStatus.current.artist || this.info.tabs[0].songtitle!=this.playerStatus.current.title) {
+                this.info.tabs[0].text=i18n("Fetching...");
+                lmsCommand("", ["musicartistinfo", "lyrics", "artist:"+this.playerStatus.current.artist, "title:"+this.playerStatus.current.title]).then(({data}) => {
+                    if (data && data.result && (data.result.lyrics || data.result.error) && data.result.artist==this.playerStatus.current.artist &&
+                        data.result.title==this.playerStatus.current.title) {
+                        this.info.tabs[0].text=data.result.lyrics ? data.result.lyrics.replace(/\n/g, "<br/>") : data.result.error;
+                        this.info.tabs[0].songartist=this.playerStatus.current.artist;
+                        this.info.tabs[0].songtitle=this.playerStatus.current.title;
+                    }
+                });
+            }
+        },
+        fetchBio() {
+            if (this.info.tabs[1].songartist!=this.playerStatus.current.artist) {
+                this.info.tabs[1].text=i18n("Fetching...");
+                lmsCommand("", ["musicartistinfo", "biography", "artist:"+this.playerStatus.current.artist]).then(({data}) => {
+                    if (data && data.result && (data.result.biography || data.result.error) && data.result.artist==this.playerStatus.current.artist) {
+                        this.info.tabs[1].text=data.result.biography ? data.result.biography.replace(/\n/g, "<br/>") : data.result.error;
+                        this.info.tabs[1].songartist=this.playerStatus.current.artist;
+                    }
+                });
+            }
+        },
+        fetchReview() {
+            if (this.info.tabs[2].songartist!=this.playerStatus.current.artist || this.info.tabs[2].songalbum!=this.playerStatus.current.album) {
+                this.info.tabs[2].text=i18n("Fetching...");
+                lmsCommand("", ["musicartistinfo", "albumreview", "artist:"+this.playerStatus.current.artist, "album:"+this.playerStatus.current.album]).then(({data}) => {
+                    if (data && data.result && (data.result.albumreview || data.result.error) && data.result.artist==this.playerStatus.current.artist &&
+                        data.result.album==this.playerStatus.current.album) {
+                        this.info.tabs[2].text=data.result.albumreview ? data.result.albumreview.replace(/\n/g, "<br/>") : data.result.error;
+                        this.info.tabs[2].songartist=this.playerStatus.current.artist;
+                        this.info.tabs[2].songalbum=this.playerStatus.current.album;
+                    }
+                });
+            }
         }
     },
     filters: {
@@ -135,6 +235,21 @@ var lmsNowPlaying = Vue.component("LmsNowPlaying", {
                 return '';
             }
             return formatSeconds(Math.floor(value));
+        }
+    },
+    watch: {
+        'info.show': function(val) {
+            // Indicate that dialog is/isn't shown, so that swipe is controlled
+            bus.$emit('dialog', 'info-dialog', val);
+        },
+        'info.tab': function(tab) {
+            if (0==tab) {
+                this.fetchLyrics();
+            } else if (1==tab) {
+                this.fetchBio();
+            } else {
+                this.fetchReview();
+            }
         }
     }
 });

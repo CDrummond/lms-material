a9fef9e16d3134a24ba2b1d097d54e2210c5e910
Craig Drummond <craig.p.drummond@gmail.com>
2018-10-23 16:06:30 +0100

    Move js, css, etc., into html sub folder - allows LMS to serve files. Also fixed skin running as default.

--- MaterialSkin/Plugin.pm
+++ MaterialSkin/Plugin.pm
@@ -1,28 +1,6 @@
 package Plugins::MaterialSkin::Plugin;
 
-use File::Basename;
-use File::Spec::Functions qw(catfile catdir);
-
-# this plugin registers the helper files (fonts, manifest) as raw downloads
-# this will allow Logitech Media Server to serve those files without a patch
 sub initPlugin {
-    my $baseDir = dirname($INC{'Plugins/MaterialSkin/Plugin.pm'});
-    my @dirs = ('HTML/material/', 'HTML/material/images', 'HTML/material/font', 'HTML/material/lang');
-    foreach $d (@dirs) {
-        my $path = catdir($baseDir, $d);
-        if (opendir(dir, $path)) {
-            my @entries = readdir(dir);
-            close(dir);
-
-            for my $file (@entries) {
-                # extend the list of file extensions if needed
-                if ($file =~ /\.(?:eot|svg|woff2?|ttf|json|png)$/) {
-                    $file = catfile($path, $file);
-                    Slim::Web::Pages->addRawDownload(basename($file), $file, Slim::Music::Info::typeFromSuffix($file));
-                }
-            }
-        }
-    }
 }
 
 1;

c21940b34728f31493c41c91cddb1b7a1c3948c2
Craig Drummond <craig.p.drummond@gmail.com>
2018-10-23 16:08:38 +0100

    Add minifying of JS and CSS - disabled for now, will enable closer to proper release.

--- mkrel.py
+++ mkrel.py
@@ -7,24 +7,34 @@
 # MIT license.
 #
 
-import sys
+import hashlib
+import os
+import re
 import requests
 import shutil
-import hashlib
+import subprocess
+import sys
+
 
 INSTALL_XML = "MaterialSkin/install.xml"
 PUBLIC_XML = "public.xml"
+BUILD_FOLDER = "build"
+MINIFY_CODE = False # Change to true when nearer a full release
+
 
 def info(s):
     print("INFO: %s" %s)
+
 
 def error(s):
     print("ERROR: %s" % s)
     exit(-1)
 
+
 def usage():
     print("Usage: %s <major>.<minor>.<patch>" % sys.argv[0])
     exit(-1)
+
 
 def checkVersion(version):
     try:
@@ -34,9 +44,11 @@ def checkVersion(version):
         patch=int(parts[2])
     except:
         error("Invalid version number")
-    
+
+
 def releaseUrl(version):
     return "https://github.com/CDrummond/lms-material/releases/download/%s/lms-material-%s.zip" % (version, version)
+
 
 def checkVersionExists(version):
     url = releaseUrl(version)
@@ -44,6 +56,7 @@ def checkVersionExists(version):
     request = requests.head(url)
     if request.status_code == 200:
         error("Version already exists")
+
 
 def updateLine(line, startStr, endStr, updateStr):
     start=line.find(startStr)
@@ -53,7 +66,8 @@ def updateLine(line, startStr, endStr, updateStr):
         if end!=-1:
             return "%s%s%s" % (line[:start], updateStr, line[end:])
     return None
-                
+
+
 def updateInstallXml(version):
     lines=[]
     updated=False
@@ -72,11 +86,129 @@ def updateInstallXml(version):
         for line in lines:
             f.write(line)
 
+
+def prepare():
+    info("Preparing code")
+    if os.path.exists(BUILD_FOLDER):
+        shutil.rmtree(BUILD_FOLDER)
+    os.mkdir(BUILD_FOLDER)
+    try:
+        shutil.copytree("MaterialSkin", "%s/MaterialSkin" % BUILD_FOLDER)
+    except Exception as e:
+        error("Failed to copy files to %s folder - %s" % (BUILD_FOLDER, str(e)))
+
+
+def cleanup():
+    info("Removing build folder")
+    if os.path.exists(BUILD_FOLDER):
+        shutil.rmtree(BUILD_FOLDER)
+
+
+def minify(version):
+    info("Minifying")
+    index = "%s/MaterialSkin/HTML/material/index.html" % BUILD_FOLDER
+    jsFiles = []
+    minifiedJs = "html/js/lms-material.min.js"
+    jsCommand = ["java", "-jar", "tools/closure-compiler/closure-compiler-v20181008.jar", "--js_output_file=%s/MaterialSkin/HTML/material/%s" % (BUILD_FOLDER, minifiedJs)]
+    replacedJs = False
+    replacedCss = False
+    fixedLines = []
+    with open(index, "r") as f:
+        lines=f.readlines()
+    for line in lines:
+        matchedJs = False
+        matchedCss = False
+        matches = re.findall('src\\s*\\=\\"html/js/[^\\"]+\\.js\\"', line)
+        if matches:
+            for match in matches:
+                path = "%s/MaterialSkin/HTML/material/%s" % (BUILD_FOLDER, match.split('"')[1])
+                jsFiles.append(path)
+                jsCommand.append(path)
+                matchedJs = True
+        else:
+            matches = re.findall('"html/css/style.css"', line)
+            if matches:
+                for match in matches:
+                    matchedCss = True
+
+        if matchedJs:
+            if not replacedJs:
+                fixedLines.append('    <script src="%s?ver=%s"></script>\n' % (minifiedJs, version))
+                replacedJs = True
+        elif matchedCss:
+            if not replacedCss:
+                fixedLines.append('    <link href="html/css/style.min.css?ver=%s" rel="stylesheet">\n' % version)
+                replacedCss = True
+        else:
+            fixedLines.append(line)
+
+    # run JS command
+    try:
+        info("    ...JS")
+        subprocess.call(jsCommand, shell=False)
+    except:
+        error("Failed to minify JS")
+
+    # run CSS command
+    origCss = "%s/MaterialSkin/HTML/material/html/css/style.css" % BUILD_FOLDER
+    minCss = "%s/MaterialSkin/HTML/material/html/css/style.min.css" % BUILD_FOLDER
+    try:
+        info("    ...CSS")
+        subprocess.call(["java", "-jar", "tools/yuicompressor/yuicompressor-2.4.8.jar", "-o", minCss, origCss], shell=False)
+    except:
+        error("Failed to minify CSS")
+
+    for f in jsFiles:
+        os.remove(f)
+    os.remove(origCss)
+
+    # Update index.html
+    with open(index, "w") as f:
+        for line in fixedLines:
+            f.write(line)
+
+
+def appendVersion(version):
+    info("Appending version")
+    index = "%s/MaterialSkin/HTML/material/index.html" % BUILD_FOLDER
+    fixedLines = []
+    with open(index, "r") as f:
+        lines=f.readlines()
+    for line in lines:
+        matchedJs = False
+        matchedCss = False
+
+        matches = re.findall('src\\s*\\=\\"html/js/[^\\"]+\\.js\\"', line)
+        if matches:
+            for match in matches:
+                fixedLines.append('    <script src="%s?ver=%s"></script>\n' % (match.split('"')[1], version))
+                matchedJs = True
+        else:
+            matches = re.findall('"html/css/style.css"', line)
+            if matches:
+                for match in matches:
+                    fixedLines.append('    <link href="html/css/style.css?ver=%s" rel="stylesheet">\n' % version)
+                    matchedCss = True
+
+        if not matchedJs and not matchedCss:
+            fixedLines.append(line)
+
+    # Update index.html
+    with open(index, "w") as f:
+        for line in fixedLines:
+            f.write(line)
+
+
 def createZip(version):
     info("Creating ZIP")
     zipFile="lms-material-%s" % version
+    os.chdir('build')
     shutil.make_archive(zipFile, 'zip', 'MaterialSkin')
-    return zipFile+".zip"
+    zipFile+=".zip"
+    os.rename(zipFile, "../%s" % zipFile)
+    os.chdir('..')
+    return zipFile
+
 
 def getSha1Sum(zipFile):
     info("Generatin SHA1")
@@ -88,6 +220,7 @@ def getSha1Sum(zipFile):
                 break
             sha1.update(data)
     return sha1.hexdigest()
+
 
 def updatePublicXml(version, zipFile, sha1):
     lines=[]
@@ -131,7 +264,14 @@ version=sys.argv[1]
 checkVersion(version)
 checkVersionExists(version)
 updateInstallXml(version)
+prepare()
+if MINIFY_CODE:
+    minify(version)
+else:
+    appendVersion(version)
+
 zipFile = createZip(version)
 sha1 = getSha1Sum(zipFile)
 updatePublicXml(version, zipFile, sha1)
+cleanup()
 

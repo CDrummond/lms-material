39997448cb571ef7c2348bf2afff4c21f83eb28c
Craig Drummond <craig.p.drummond@gmail.com>
2018-10-30 22:43:13 +0000

    Add basic info dialog; shows server stats, players, and allows library to be rescanned.

---
+++ MaterialSkin/HTML/material/html/js/information.js
@@ -0,0 +1,149 @@
+/*
+ * LMS-Material
+ *
+ * Copyright (c) 2018 Craig Drummond <craig.p.drummond@gmail.com>
+ * MIT license.
+ */
+
+Vue.component('lms-information-dialog', {
+    template: `
+      <v-dialog v-model="show" fullscreen app>
+        <v-card>
+          <v-toolbar color="primary" dark app class="lms-toolbar">
+            <v-btn flat icon @click.native="close()"><v-icon>arrow_back</b-icon></v-btn>
+            <v-toolbar-title>{{title}}</v-toolbar-title>
+          </v-toolbar>
+          <div class="settings-toolbar-pad"></div>
+          <div class="settings-list">
+
+            <p class="about-header">{{i18n('Library')}}</p>
+            <template v-for="(item, index) in library">
+              <p class="about-indent">{{item}}</p>
+            </template>
+            <v-menu bottom left>
+              <v-btn slot="activator" flat>Rescan</v-btn>
+              <v-list>
+                <template v-for="(item, index) in rescans">
+                  <v-list-tile @click="initiateScan(item.prompt, item.command)">
+                    <v-list-tile-title>{{item.title}}</v-list-tile-title>
+                  </v-list-tile>
+                </template>
+              </v-list>
+            </v-menu>
+            <div style="height:24px"></div>
+
+            <p class="about-header">{{i18n('Players')}}</p>
+            <template v-for="(item, index) in players">
+              <p class="about-indent">{{item.name}}
+                <i> <template v-for="(info, index) in item.info">{{info}}</template></i>
+              </p>
+            </template>
+          </div>
+        </v-card>
+      </v-dialog>
+`,
+    props: [],
+    data() {
+        return {
+            show: false,
+            title: "Information", // i18n not rquired, as should not be seen
+            library: [],
+            players: [],
+            rescans: [ {title:undefined, prompt:undefined, command: ["wipecache"]},
+                       {title:undefined, prompt:undefined, command: ["rescan"]},
+                       {title:undefined, prompt:undefined, command: ["rescan", "playlists"]} ]
+        }
+    },
+    mounted() {
+        bus.$on('toolbarAction', function(act) {
+            if (act==TB_INFO.id) {
+
+                bus.$emit('dialog', 'information', true);
+                this.update();
+                this.timer = setInterval(function () {
+                    this.update();
+                }.bind(this), 2000);
+                this.show = true;
+            }
+        }.bind(this));
+
+        bus.$on('closeDialog', function() {
+            if (this.show) {
+                this.close();
+            }
+        }.bind(this));
+
+        bus.$on('langChanged', function() {
+            this.initItems();
+        }.bind(this));
+        this.initItems();
+    },
+    methods: {
+        initItems() {
+            this.rescans[0].title=i18n("Full rescan");
+            this.rescans[0].prompt=i18n("Clear library, and rescan everything?");
+            this.rescans[1].title=i18n("Update rescan");
+            this.rescans[1].prompt=i18n("Look for new, and modified, files?");
+            this.rescans[2].title=i18n("Update playlists");
+            this.rescans[2].prompt=i18n("Rescan for playlist changes?");
+        },
+        update() {
+            lmsCommand("", ["serverstatus", 0, LMS_MAX_PLAYERS]).then(({data}) => {
+                if (data && data.result) {
+                    this.players = [];
+                    if (data.result.players_loop) {
+                         data.result.players_loop.forEach(i => {
+                            var info = [ i18n("Model: %1", i.modelname),
+                                         i18n("Type: %1", i.model),
+                                         i18n("Firmware: %1", i.firmware),
+                                         i18n("IP: %1", i.ip.split(':')[0]),
+                                         i18n("Mac Address: %1", i.playerid) ];
+
+                            this.players.push({name: i.name, info: info});
+                        });
+                    }
+
+                    this.library=[ i18n("Total genres: %1", data.result["info total genres"]),
+                                   i18n("Total artists: %1", data.result["info total artists"]),
+                                   i18n("Total albums: %1", data.result["info total albums"]),
+                                   i18n("Total songs: %1", data.result["info total songs"]),
+                                   i18n("Total duration: %1", formatSeconds(data.result["info total duration"])),
+                                   i18n("Last scan: %1", undefined==data.result.lastscan || 1==data.result.scanning 
+                                        ? i18n("In progress") : formatDate(data.result.lastscan))];
+
+                    if (data.result.lastscanfailed) {
+                        this.library.push("Last scan failure: %1", data.result.lastscanfailed);
+                    }
+
+                    this.title=i18n("SlimServer v%1", data.result.version);
+                }
+            });
+        },
+        close() {
+            this.show = false;
+            clearInterval(this.timer);
+            this.timer = undefined;
+        },
+        initiateScan(prompt, command) {
+            this.$confirm(prompt, {buttonTrueText: i18n('Rescan'), buttonFalseText: i18n('Cancel')}).then(res => {
+                if (res) {
+                    lmsCommand("", command)
+                }
+            });
+        },
+        i18n(str) {
+            if (this.show) {
+                return i18n(str);
+            } else {
+                return str;
+            }
+        }
+    },
+    beforeDestroy() {
+        if (undefined!==this.timer) {
+            clearInterval(this.timer);
+            this.timer = undefined;
+        }
+    }
+})
+

9da5da6ddcad269d8a74f52d1bd2b27e0d3fa1b4
Craig Drummond <craig.p.drummond@gmail.com>
2018-11-12 00:05:50 +0000

    Initial desktop UI files - non functional, and in no way useable!

---
+++ MaterialSkin/HTML/material/html/js/main-desktop.js
@@ -0,0 +1,34 @@
+/**
+ * LMS-Material
+ *
+ * Copyright (c) 2018 Craig Drummond <craig.p.drummond@gmail.com>
+ * MIT license.
+ */
+
+Vue.use(VueLazyload);
+
+var app = new Vue({
+    el: '#app',
+    data() {
+    },
+    created() {
+        // For testing, allow pages to be served by (e.g.) python -m SimpleHTTPServer. Use http://localhost:8000/?lms=<real address of LMS>
+        var res = RegExp('[?&]lms=([^&#]*)').exec(window.location.href);
+        if (res && 2==res.length) {
+            lmsServerAddress = "http://"+res[1]+":9000";
+        }
+        this.$store.commit('initUiSettings');
+        initApp();
+    },
+    computed: {
+        darkUi() {
+            return this.$store.state.darkUi;
+        },
+        lang() {
+            return this.$store.state.lang;
+        }
+    },
+    store,
+    lmsServer
+})
+

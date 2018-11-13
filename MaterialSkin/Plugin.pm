9368bb38f21b21849068ad526adeff8bbe7a7fb4
Michael Herger <craig.p.drummond@gmail.com>
2018-11-13 06:01:00 +0100

    Create /desktop pagehandler in plugin. Move template to main folder.

--- MaterialSkin/Plugin.pm
+++ MaterialSkin/Plugin.pm
@@ -1,6 +1,11 @@
 package Plugins::MaterialSkin::Plugin;
 
 sub initPlugin {
+    if (main::WEBUI) {
+        Slim::Web::Pages->addPageFunction( 'desktop', sub {
+            return Slim::Web::HTTP::filltemplatefile('desktop.html', $_[1]);
+        } );
+    }
 }
 
 1;

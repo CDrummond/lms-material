f474211bcd108ef8007ab238e0d87d3a3cab1878
Craig Drummond <craig.p.drummond@gmail.com>
2018-11-13 19:36:16 +0000

    Request HTML info

--- MaterialSkin/HTML/material/html/js/nowplaying-page.js
+++ MaterialSkin/HTML/material/html/js/nowplaying-page.js
@@ -246,7 +246,7 @@ var lmsNowPlaying = Vue.component("lms-now-playing", {
         fetchLyrics() {
             if (this.info.tabs[0].songartist!=this.playerStatus.current.artist || this.info.tabs[0].songtitle!=this.playerStatus.current.title) {
                 this.info.tabs[0].text=i18n("Fetching...");
-                lmsCommand("", ["musicartistinfo", "lyrics", "artist:"+this.playerStatus.current.artist, "title:"+this.playerStatus.current.title]).then(({data}) => {
+                lmsCommand("", ["musicartistinfo", "lyrics", "artist:"+this.playerStatus.current.artist, "title:"+this.playerStatus.current.title, "html:1"]).then(({data}) => {
                     if (data && data.result && (data.result.lyrics || data.result.error) && data.result.artist==this.playerStatus.current.artist &&
                         data.result.title==this.playerStatus.current.title) {
                         this.info.tabs[0].text=data.result.lyrics ? replaceNewLines(data.result.lyrics) : data.result.error;
@@ -259,7 +259,7 @@ var lmsNowPlaying = Vue.component("lms-now-playing", {
         fetchBio() {
             if (this.info.tabs[1].songartist!=this.playerStatus.current.artist) {
                 this.info.tabs[1].text=i18n("Fetching...");
-                lmsCommand("", ["musicartistinfo", "biography", "artist:"+this.playerStatus.current.artist]).then(({data}) => {
+                lmsCommand("", ["musicartistinfo", "biography", "artist:"+this.playerStatus.current.artist, "html:1"]).then(({data}) => {
                     if (data && data.result && (data.result.biography || data.result.error) && data.result.artist==this.playerStatus.current.artist) {
                         this.info.tabs[1].text=data.result.biography ? replaceNewLines(data.result.biography) : data.result.error;
                         this.info.tabs[1].songartist=this.playerStatus.current.artist;
@@ -270,7 +270,7 @@ var lmsNowPlaying = Vue.component("lms-now-playing", {
         fetchReview() {
             if (this.info.tabs[2].songartist!=this.playerStatus.current.artist || this.info.tabs[2].songalbum!=this.playerStatus.current.album) {
                 this.info.tabs[2].text=i18n("Fetching...");
-                lmsCommand("", ["musicartistinfo", "albumreview", "artist:"+this.playerStatus.current.artist, "album:"+this.playerStatus.current.album]).then(({data}) => {
+                lmsCommand("", ["musicartistinfo", "albumreview", "artist:"+this.playerStatus.current.artist, "album:"+this.playerStatus.current.album, "html:1"]).then(({data}) => {
                     if (data && data.result && (data.result.albumreview || data.result.error) && data.result.artist==this.playerStatus.current.artist &&
                         data.result.album==this.playerStatus.current.album) {
                         this.info.tabs[2].text=data.result.albumreview ? replaceNewLines(data.result.albumreview) : data.result.error;

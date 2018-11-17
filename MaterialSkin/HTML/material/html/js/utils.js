78b41941cbe91631fa516fd5c92a3270fab568ec
Craig Drummond <craig.p.drummond@gmail.com>
2018-11-17 23:00:38 +0000

    Fix durations longer than 24hrs.

--- MaterialSkin/HTML/material/html/js/utils.js
+++ MaterialSkin/HTML/material/html/js/utils.js
@@ -9,7 +9,7 @@ var bus = new Vue();
 
 function formatSeconds(secs) {
     var sec_num = parseInt(secs, 10)    
-    var hours   = Math.floor(sec_num / 3600) % 24
+    var hours   = Math.floor(sec_num / 3600);
     var minutes = Math.floor(sec_num / 60) % 60
     var seconds = sec_num % 60
     if (hours>0) {

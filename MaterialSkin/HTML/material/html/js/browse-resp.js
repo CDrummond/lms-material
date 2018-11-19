5e17c98490bc8d649b56c37ee4bf8a3a24776d8a
Craig Drummond <craig.p.drummond@gmail.com>
2018-11-19 19:50:41 +0000

    Remove "this.playerStatus.current.title" from translations!

--- MaterialSkin/HTML/material/html/js/browse-resp.js
+++ MaterialSkin/HTML/material/html/js/browse-resp.js
@@ -682,7 +682,7 @@ function parseBrowseResp(data, parent, options, idStart) {
     }
 
     } catch(e) {
-        resp.items.push({title:i18n("ERROR: List processing failed\n%1", e), type: 'text', id:'error'});
+        resp.items.push({title:i18n("ERROR: List processing failed")+"\n"+e, type: 'text', id:'error'});
     }
 
     return resp;

74a1bcabe17d3a85f3555f0a01c581c73e39aa6f
Craig Drummond <craig.p.drummond@gmail.com>
2018-12-17 17:00:01 +0000

    Add currentcover class to store current cover

--- MaterialSkin/HTML/material/html/js/nowplaying-page.js
+++ MaterialSkin/HTML/material/html/js/nowplaying-page.js
@@ -160,7 +160,7 @@ var lmsNowPlaying = Vue.component("lms-now-playing", {
                  playerStatus: {
                     ison: 1,
                     isplaying: 1,
-                    current: { canseek:1, artwork_url:undefined, coverid: undefined, duration:0, time:0, title:undefined, artist:undefined, 
+                    current: { canseek:1, duration:0, time:0, title:undefined, artist:undefined, 
                                album:undefined, albumName:undefined, technicalInfo: "" },
                     playlist: { shuffle:0, repeat: 0 },
                  },
@@ -179,58 +179,6 @@ var lmsNowPlaying = Vue.component("lms-now-playing", {
         }
         this.info.sync=getLocalStorageBool("syncInfo", true);
         bus.$on('playerStatus', function(playerStatus) {
-
-            // Has cover changed?
-            var coverUrl = this.cover;
-
-            if (playerStatus.playlist.count == 0) {
-                if (undefined===this.coverFromInfo || this.coverFromInfo || undefined==this.cover) {
-                    coverUrl=resolveImage("music/0/cover");
-                    this.coverFromInfo = false;
-                }
-            } else if (playerStatus.current.artwork_url!=this.playerStatus.current.artwork_url ||
-                playerStatus.current.coverid!=this.playerStatus.current.coverid ||
-                this.coverFromPlayer!=this.$store.state.player.id) {
-                this.playerStatus.current.artwork_url = playerStatus.current.artwork_url;
-                this.playerStatus.current.coverid = playerStatus.current.coverid;
-                this.coverFromPlayer = this.$store.state.player.id
-
-                coverUrl = undefined;
-                if (this.playerStatus.current.artwork_url) {
-                    coverUrl=resolveImage(null, this.playerStatus.current.artwork_url);
-                }
-                if (undefined==coverUrl && this.playerStatus.current.coverid) {
-                    coverUrl=lmsServerAddress+"/music/"+this.playerStatus.current.coverid+"/cover.jpg";
-                }
-                if (undefined==coverUrl) {
-                    // Use players current cover as cover image. Need to add extra (coverid, etc) params so that
-                    // the URL is different between tracks...
-                    coverUrl=lmsServerAddress+"/music/current/cover.jpg?player=" + this.$store.state.player.id;
-                    if (playerStatus.current.coverid) {
-                        coverUrl+="&coverid="+playerStatus.current.coverid;
-                    } else {
-                        if (playerStatus.current.album_id) {
-                            coverUrl+="&album_id="+playerStatus.current.album_id;
-                        } else {
-                            if (playerStatus.current.album) {
-                                coverUrl+="&album="+encodeURIComponent(playerStatus.current.album);
-                            }
-                            if (playerStatus.current.albumartist) {
-                                coverUrl+="&artist="+encodeURIComponent(playerStatus.current.albumartist);
-                            }
-                            if (playerStatus.current.year && playerStatus.current.year>0) {
-                                coverUrl+="&year="+playerStatus.current.year;
-                            }
-                        }
-                    }
-                }
-                this.coverFromInfo = true;
-            }
-
-            if (coverUrl!=this.cover) {
-                this.cover = coverUrl;
-            }
-
             var playStateChanged = false;
             var trackChanged = false;
             // Have other items changed
@@ -324,6 +272,10 @@ var lmsNowPlaying = Vue.component("lms-now-playing", {
         }.bind(this));
         // Refresh status now, in case we were mounted after initial status call
         bus.$emit('refreshStatus');
+
+        bus.$on('coverChanged', function(coverUrl) {
+            this.cover = coverUrl;
+        }.bind(this));
 
         bus.$on('langChanged', function() {
             this.initItems();

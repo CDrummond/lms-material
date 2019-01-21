86250f2dcda7cf1d583eefa5a861755b87aea004
Craig Drummond <craig.p.drummond@gmail.com>
2019-01-21 16:12:57 +0000

    Clear browse history before fetching artist info - otherwise stack could get quite large.

--- MaterialSkin/HTML/material/html/js/browse-page.js
+++ MaterialSkin/HTML/material/html/js/browse-page.js
@@ -848,6 +848,7 @@ var lmsBrowse = Vue.component("lms-browse", {
                 this.fetchItems(this.buildCommand(item, act), item);
             } else if (act===MORE_LIB_ACTION.cmd) {
                 if (item.id) {
+                    this.goHome();
                     if (item.id.startsWith("artist_id:")) {
                         this.fetchItems({command: ["artistinfo", "items"], params: ["menu:1", item.id, "html:1"]}, item);
                     } else if (item.id.startsWith("album_id:")) {

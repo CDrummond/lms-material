ff688f87d0df9e6eeb73f8dc0aab7b3b7b56742c
Craig Drummond <craig.p.drummond@gmail.com>
2019-02-02 18:29:00 +0000

    Exclude genreinfo for addall/playall addition too

--- MaterialSkin/HTML/material/html/js/browse-page.js
+++ MaterialSkin/HTML/material/html/js/browse-page.js
@@ -568,7 +568,7 @@ var lmsBrowse = Vue.component("lms-browse", {
                 // No menu actions? If first item is playable, add a PlayAll/AddAll to toolbar...
                 if ((!item.id || !item.id.startsWith(TOP_ID_PREFIX)) && this.menuActions.length==0 && this.items.length>0 && this.items[0].menuActions &&
                    !(this.command.command.length>0 && (this.command.command[0]=="trackinfo" || this.command.command[0]=="artistinfo" ||
-                                                       this.command.command[0]=="albuminfo"))) {
+                                                       this.command.command[0]=="albuminfo") || this.command.command[0]=="genreinfo")) {
                     this.items[0].menuActions.forEach(i => {
                         if (i.cmd==ADD_ACTION.cmd || i.cmd==PLAY_ACTION.cmd) {
                             this.menuActions=[ADD_ALL_ACTION, PLAY_ALL_ACTION];

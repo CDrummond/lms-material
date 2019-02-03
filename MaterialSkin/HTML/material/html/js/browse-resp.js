f657ef0e0758648758ca2bcdd9660fb39ac7c750
Craig Drummond <craig.p.drummond@gmail.com>
2019-02-03 17:41:58 +0000

    Fix scrolling in A..Z list (due to cache usage)

--- MaterialSkin/HTML/material/html/js/browse-resp.js
+++ MaterialSkin/HTML/material/html/js/browse-resp.js
@@ -187,8 +187,8 @@ function parseBrowseResp(data, parent, options, idStart, cacheKey) {
                 resp.items.push(prevItem);
             }
 
-            data.result.count=resp.items.length;
-            resp.subtitle=i18np("1 Category", "%1 Categories", data.result.count);
+            resp.total=resp.items.length;
+            resp.subtitle=i18np("1 Category", "%1 Categories", resp.total);
         } else if (data.result.item_loop) {  // SlimBrowse response
             var playAction = false;
             var addAction = false;

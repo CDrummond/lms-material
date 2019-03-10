75759d9fe91a2afc9e62412c14483c43666fbcd7
Craig Drummond <craig.p.drummond@gmail.com>
2019-03-10 14:04:16 +0000

    Fix pinned apps.

--- MaterialSkin/HTML/material/html/js/browse-page.js
+++ MaterialSkin/HTML/material/html/js/browse-page.js
@@ -702,7 +702,7 @@ var lmsBrowse = Vue.component("lms-browse", {
             if ("search"==item.type || "entry"==item.type) {
                 return;
             }
-            if ("text"==item.type || undefined==item.type) {
+            if ("text"==item.type || (undefined==item.type && undefined==item.group)) { // if group is not undefined, its probably a pinned app
                 if (this.canClickText(item)) {
                     this.doTextClick(item);
                 }

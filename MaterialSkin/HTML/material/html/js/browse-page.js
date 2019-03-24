cbdb2b7d5f0ce91fc70503ab2c46456be3801b72
Craig Drummond <craig.p.drummond@gmail.com>
2019-03-24 07:59:25 +0000

    Stop drag'n'drop scrolling when reach top/bottom of list.

--- MaterialSkin/HTML/material/html/js/browse-page.js
+++ MaterialSkin/HTML/material/html/js/browse-page.js
@@ -1781,7 +1781,11 @@ var lmsBrowse = Vue.component("lms-browse", {
             ev.preventDefault(); // Otherwise drop is never called!
         },
         scrollList(step) {
-            setScrollTop(this.scrollElement, this.scrollElement.scrollTop + step);
+            var pos = this.scrollElement.scrollTop + step;
+            setScrollTop(this.scrollElement, pos);
+            if (pos<=0 || pos>=this.scrollElement.scrollTopMax) {
+                this.stopScrolling = true;
+            }
             if (!this.stopScrolling) {
                 setTimeout(function () {
                     this.scrollList(step);

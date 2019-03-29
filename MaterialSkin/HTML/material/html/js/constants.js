87b917aa8a80776685ffa7439dc49703fb4de450
Craig Drummond <craig.p.drummond@gmail.com>
2019-03-29 16:56:57 +0000

    Reduce space used to cache artist, and album, lists.

--- MaterialSkin/HTML/material/html/js/constants.js
+++ MaterialSkin/HTML/material/html/js/constants.js
@@ -22,4 +22,4 @@ const LMS_AZ_MAX_PER_LETTER = 300;
 const LMS_MATERIAL_UI_DEFAULT_PREF = "plugin.material-skin:defaults";
 const LMS_MATERIAL_DEFAULT_PINNED_PREF = "plugin.material-skin:pinned";
 const LMS_VOLUME_CLOSE_TIMEOUT = 10000;
-const LMS_CACHE_VERSION = 1;
+const LMS_CACHE_VERSION = 3;

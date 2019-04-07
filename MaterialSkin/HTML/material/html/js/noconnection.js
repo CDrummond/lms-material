9d83fcb2a4eff254edccfe2eea5c6412a4ca51b2
Craig Drummond <craig.p.drummond@gmail.com>
2019-04-07 15:15:34 +0100

    Disable reconnect button for 10 seconds after pressed

--- MaterialSkin/HTML/material/html/js/noconnection.js
+++ MaterialSkin/HTML/material/html/js/noconnection.js
@@ -12,14 +12,14 @@ Vue.component('lms-noconnection', {
   <table>
    <tr><td style="text-align: center; padding-bottom: 32px;"><h2>{{i18n('Server connection lost...')}}</h2></td></tr>
    <tr><td style="text-align: center;"><v-progress-circular color="primary" size=72 width=6 indeterminate></v-progress-circular></td></tr>
-   <tr><td style="text-align: center;"><v-btn @click="reconnect()">{{i18n('Reconnect'}}</v-btn></td></tr>
+   <tr><td style="text-align: center;"><v-btn @click="reconnect()" :disabled="disabled">{{i18n('Reconnect'}}</v-btn></td></tr>
   </table>
  </v-card>
 </v-dialog>
 `,
     props: [],
     data () {
-        return { show:false }
+        return { show:false, disabled:false }
     },
     computed: {
         noNetwork () {
@@ -27,16 +27,36 @@ Vue.component('lms-noconnection', {
         }
     },
     mounted() {
+        this.pressTimer = undefined;
         bus.$on('networkStatus', function(connected) {
+            this.disabled = false;
+            this.cancelTimer();
             this.show = !connected;
         }.bind(this));
+    },
+    beforeDestroy() {
+        this.cancelTimer();
     },
     methods: {
         i18n(str) {
             return this.show ? i18n(str) : str;
         },
         reconnect() {
-            bus.$emit("reconnect");
+            if (undefined==this.pressTimer) {
+                bus.$emit("reconnect");
+                this.disabled = true;
+                this.pressTimer = setTimeout(function () {
+                    this.pressTimer = undefined;
+                    this.disabled = false;
+                }.bind(this), 10*1000);
+            }
+        },
+        cancelTimer() {
+             if (undefined!==this.pressTimer) {
+                clearTimeout(this.pressTimer);
+                this.pressTimer = undefined;
+                this.disabled = false;
+            }
         }
     }
 })

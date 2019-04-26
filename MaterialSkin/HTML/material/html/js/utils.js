462956cd1643437af66bc69a6c7d4a435d240aca
Craig Drummond <craig.p.drummond@gmail.com>
2019-04-26 16:25:29 +0100

    Context menu for dynamic playlists.

--- MaterialSkin/HTML/material/html/js/utils.js
+++ MaterialSkin/HTML/material/html/js/utils.js
@@ -465,7 +465,7 @@ function checkRemoteTitle(item) {
 }
 
 function hasPlayableId(item) {
-    return item.item_id || item.track || item.track_id || item.album_id || item.artist_id ||
+    return item.item_id || item.track || item.track_id || item.album_id || item.artist_id || item.album || item.playlistid /* dynamic playlists*/ ||
            item.album || item.artist || item.variousartist || item.year || item.genre || item.playlist; // CustomBrowse
 }
 

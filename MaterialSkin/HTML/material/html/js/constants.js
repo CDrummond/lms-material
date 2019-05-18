7d1f272d89e2266d342abd456b50e43f070e62ba
Craig Drummond <craig.p.drummond@gmail.com>
2019-05-18 13:30:37 +0100

    Use view-recycler for all lists of more than 200 items.

--- MaterialSkin/HTML/material/html/js/constants.js
+++ MaterialSkin/HTML/material/html/js/constants.js
@@ -8,16 +8,12 @@
 
 const LMS_BATCH_SIZE = 25000;
 const LMS_QUEUE_BATCH_SIZE = 500;
-const LMS_MIN_GRID_SCROLLER_ITEMS = 300;
-const LMS_MIN_LIST_SCROLLER_ITEMS = 100;
-const LMS_MAX_QUEUE_NON_RECYLER_ITEMS = 250;
+const LMS_MAX_NON_SCROLLER_ITEMS = 200;
 const LMS_MAX_PLAYERS = 100;
 const LMS_LIST_IMAGE_SIZE = "_100x100_o";
 const LMS_GRID_IMAGE_SIZE = "_300x300_f";
 const LMS_DEFAULT_LIBRARY = "0";
 const LMS_SKIN_LANGUAGES = new Set(['de', 'en', 'en-gb', 'fr', 'nl']);
-const LMS_AZ_MAX_SIZE = 10;
-const LMS_AZ_MAX_PER_LETTER = 300;
 const LMS_MATERIAL_UI_DEFAULT_PREF = "plugin.material-skin:defaults";
 const LMS_MATERIAL_DEFAULT_PINNED_PREF = "plugin.material-skin:pinned";
 const LMS_VOLUME_CLOSE_TIMEOUT = 10000;

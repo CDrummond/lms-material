50dcb15a5cb6a410604a7f535cf82258dfd45153
Craig Drummond <craig.p.drummond@gmail.com>
2019-05-24 16:25:57 +0100

    Take ratings/techInfo setting into account

--- MaterialSkin/HTML/material/html/js/nowplaying-page.js
+++ MaterialSkin/HTML/material/html/js/nowplaying-page.js
@@ -499,7 +499,8 @@ var lmsNowPlaying = Vue.component("lms-now-playing", {
                 this.portraitPad = 0;
             } else {
                 var coverMax = this.portraitElem.offsetWidth-/*pad*/32;
-                var topAndBotSpace = (this.portraitElem.offsetHeight - (coverMax + /*bottom*/120 + /*text*/80))/2;
+                var topAndBotSpace = (this.portraitElem.offsetHeight -
+                                        (coverMax + /*bottom*/(this.$store.state.ratingsSupport || this.$store.state.techInfo ? 120 : 90) + /*text*/80))/2;
                 var portraitPad = Math.max(0, Math.floor(topAndBotSpace/2)-8);
                 if (portraitPad!=this.portraitPad) {
                     this.portraitPad = portraitPad;

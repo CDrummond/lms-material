4f90cd051a565d46e2f58f4390a94719e0b4314c
Craig Drummond <craig.p.drummond@gmail.com>
2019-06-03 19:50:58 +0100

    Hacky work-around for group player appearing in serverstatus *after* its been removed!

--- MaterialSkin/HTML/material/html/js/manage-players.js
+++ MaterialSkin/HTML/material/html/js/manage-players.js
@@ -311,6 +311,12 @@ Vue.component('lms-manage-players', {
                 if (res) {
                     lmsCommand("", ['playergroups', 'delete', 'id:'+player.id]).then(({data}) => {
                         bus.$emit('refreshServerStatus');
+                        // Group player seems to appear in server status update after its removed! Checking again, and its gone!
+                        if (player.isgroup) {
+                            setTimeout(function () {
+                                bus.$emit('refreshServerStatus');
+                            }.bind(this), 100);
+                        }
                     });
                 }
             });

870ad5ee14e9471a5b2619d887b4fd22b8b0e9d4
Craig Drummond <craig.p.drummond@gmail.com>
2019-08-01 22:43:45 +0100

    Work-around filesystem JSONRPC API returing "isfolder:null" for folders with non-ascii characters.

--- MaterialSkin/HTML/material/html/js/browse-resp.js
+++ MaterialSkin/HTML/material/html/js/browse-resp.js
@@ -705,8 +705,9 @@ function parseBrowseResp(data, parent, options, idStart, cacheKey) {
             var haveFiles = false;
             for (var idx=0, loop=data.result.fsitems_loop, loopLen=loop.length; idx<loopLen; ++idx) {
                 var i = loop[idx];
-                var isFolder = parseInt(i.isfolder)==1;
+                var isFolder = parseInt(i.isfolder)==1 || i.isfolder==null;
                 var name = i.name;
+
                 if (isFolder) {
                     haveFolders = true;
                     if (!i.name || i.name.length<1) {
@@ -723,17 +724,16 @@ function parseBrowseResp(data, parent, options, idStart, cacheKey) {
                     }
                     haveFiles = true;
                 }
-                var fixedPath = i.path.replace("\\", "\\\\");
                 var key = name.charAt(0).toUpperCase();
                 if (undefined!=key && (resp.jumplist.length==0 || resp.jumplist[resp.jumplist.length-1].key!=key || resp.jumplist[resp.jumplist.length-1].folder!=isFolder)) {
                     resp.jumplist.push({key: key, index: resp.items.length+idStart, folder:isFolder});
                 }
                 resp.items.push({
-                              id: fixedPath,
+                              id: i.path,
                               title: name,
                               subtitle: undefined,
                               command: isFolder ? ["readdirectory"] : [],
-                              params: isFolder ? ["folder:"+fixedPath] : [],
+                              params: isFolder ? [i.isfolder==null ? data.params[1][3]+"/"+name : ("folder:"+i.path.replace("\\", "\\\\"))] : [],
                               menu: [PLAY_ACTION, INSERT_ACTION, ADD_ACTION],
                               type: isFolder ? "group" : "track",
                               icon: isFolder ? "folder" : "music_note",

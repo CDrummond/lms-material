3672ef72fca5bf1be55b90fd69bf8af11fa2918c
Craig Drummond <craig.p.drummond@gmail.com>
2019-08-11 09:34:57 +0100

    Correctly update power state of other players.

--- MaterialSkin/HTML/material/html/js/store.js
+++ MaterialSkin/HTML/material/html/js/store.js
@@ -138,6 +138,10 @@ const store = new Vuex.Store({
             }
 
             if (!changed) {
+                for (var i=0, len=state.players.length; i<len; ++i) {
+                    state.players[i].ison = players[i].ison;
+                    state.players[i].isconnected = players[i].isconnected;
+                }
                 return;
             }
 

82856370a021821133c5f47fd190163d83b43a2a
Craig Drummond <craig.p.drummond@gmail.com>
2019-08-22 17:42:45 +0100

    Allow drag'n'drop re-ordering of top-level browse items.

--- MaterialSkin/HTML/material/html/js/utils.js
+++ MaterialSkin/HTML/material/html/js/utils.js
@@ -707,3 +707,14 @@ function showMenu(obj, newMenu) {
         obj.menu = newMenu;
     }
 }
+
+function arrayMove(arr, from, to) {
+    if (to >= arr.length) {
+        var k = to - arr.length + 1;
+        while (k--) {
+            arr.push(undefined);
+        }
+    }
+    arr.splice(to, 0, arr.splice(from, 1)[0]);
+    return arr;
+};

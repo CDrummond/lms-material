eb950de446d70be21aedf518e5711e33ec0691da
Craig Drummond <craig.drummond@roke.co.uk>
2019-08-23 12:02:11 +0100

    Presets, not Pesets

--- MaterialSkin/HTML/material/html/js/ui-settings.js
+++ MaterialSkin/HTML/material/html/js/ui-settings.js
@@ -289,7 +289,7 @@ Vue.component('lms-ui-settings', {
 
             this.showItems=[{id: TOP_MYMUSIC_ID, name:i18n("My Music"), show:!this.hidden.has(TOP_MYMUSIC_ID)},
                             {id: TOP_FAVORITES_ID, name:i18n("Favorites"), show:!this.hidden.has(TOP_FAVORITES_ID)},
-                            {id: TOP_PRESETS_ID, name:i18n("Pesets"), show:!this.hidden.has(TOP_PRESETS_ID)},
+                            {id: TOP_PRESETS_ID, name:i18n("Presets"), show:!this.hidden.has(TOP_PRESETS_ID)},
                             {id: TOP_APPS_ID, name:i18n("Apps"), show:!this.hidden.has(TOP_APPS_ID)},
                             {id: TOP_RADIO_ID, name:i18n("Radio"), show:!this.hidden.has(TOP_RADIO_ID)}];
 

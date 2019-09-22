6be0d6201ab5410a671e67a24f6469afe25fb6f9
Craig Drummond <craig.p.drummond@gmail.com>
2019-09-22 06:35:51 +0100

    Add dialog!

---
+++ MaterialSkin/HTML/material/html/js/podcast-add-dialog.js
@@ -0,0 +1,147 @@
+/**
+ * LMS-Material
+ *
+ * Copyright (c) 2018-2019 Craig Drummond <craig.p.drummond@gmail.com>
+ * MIT license.
+ */
+
+Vue.component('lms-podcast-add-dialog', {
+    template: `
+<v-dialog scrollable v-model="show" persistent width="600">
+ <v-card>
+  <v-card-title>{{i18n("Add podcast")}}</v-card-title>
+  <v-form ref="form" v-model="valid" lazy-validation>
+   <v-list two-line>
+    <v-list-tile>
+     <v-list-tile-content>
+      <v-text-field clearable v-if="show" :label="i18n('Name')" v-model="name" class="lms-search" :rules="nameRules" required></v-text-field>
+     </v-list-tile-content>
+    </v-list-tile>
+    <v-list-tile>
+     <v-list-tile-content>
+      <v-text-field clearable autofocus :label="i18n('URL')" v-model="url" class="lms-search" :rules="urlRules" required></v-text-field>
+     </v-list-tile-content>
+     <v-list-tile-action><v-btn flat @click="validate">{{i18n('Validate')}}</v-btn></v-list-tile-action>
+    </v-list-tile>
+    <v-list-tile v-if="error">
+     <p style="color:red">{{error}}</p>
+    </v-list-tile>
+   </v-list>
+  </v-form>
+  <v-card-actions>
+   <v-spacer></v-spacer>
+   <v-btn flat @click.native="cancel()">{{i18n('Cancel')}}</v-btn>
+   <v-btn flat @click.native="add()">{{i18n('Add')}}</v-btn>
+  </v-card-actions>
+ </v-card>
+</v-dialog>
+`,
+    props: [],
+    data() {
+        return {
+            valid: false,
+            show: false,
+            name: "",
+            url: "",
+            item: undefined,
+            error: undefined,
+            nameRules: [
+                v => !!v || i18n('Name is required'),
+                v => (v && v.trim().length > 0) || i18n('Name is required')
+            ],
+            urlRules: [
+                v => !!v || i18n('URL is required'),
+                v => (v && v.trim().length > 0) || i18n('URL is required')
+            ]
+        }
+    },
+    mounted() {
+        bus.$on('podcastadd.open', function() {
+            this.url = "";
+            this.name = "";
+            this.show = true;
+        }.bind(this));
+        bus.$on('esc', function() {
+            this.cancel();
+        }.bind(this));
+    },
+    methods: {
+        cancel() {
+            this.show=false;
+            this.cancelErrorTimer();
+            this.parser=undefined;
+        },
+        validate() {
+            var url = this.url ? this.url.trim() : "";
+            if (url.length>4) {
+                this.error = undefined;
+                this.cancelErrorTimer();
+                axios.get(url).then(({data}) => {
+                    this.parseResp(data);
+                }).catch(err => {
+                    // CORS error?
+                    axios.get('https://cors-anywhere.herokuapp.com/'+url).then(({data}) => {
+                        this.parseResp(data);
+                    }).catch(err2 => {
+                        this.showError(err2);
+                    });
+                });
+            } else {
+                this.showError(i18n("Invalid URL"));
+            }
+        },
+        parseResp(data) {
+            try {
+                if (undefined==this.parser) {
+                    this.parser = new DOMParser();
+                }
+                var xmlDoc = this.parser.parseFromString(data, "text/xml");
+                this.name = xmlDoc.getElementsByTagName("title")[0].childNodes[0].nodeValue;
+            } catch(e) {
+                this.showError(i18n("Failed to parse feed"));
+            }            
+        },
+        add() {
+            var url = this.url ? this.url.trim() : "";
+            var name = this.name ? this.name.trim() : "";
+            if (url.length<1 || name.length<1) {
+                return;
+            }
+            lmsCommand("", ["material-skin", "add-podcast", "url:"+url, "name:"+name]).then(({datax}) => {
+                bus.$emit('refreshList', SECTION_PODCASTS);
+                this.show=false;
+            }).catch(err => {
+                this.showError(i18n("Failed add podcast"));
+            });
+        },
+        showError(str) {
+            this.error = str;
+            this.cancelErrorTimer();
+            this.errorTimer = setTimeout(function () {
+                this.error = undefined;
+                this.errorTimer = undefined;
+            }.bind(this), 2500);
+        },
+        cancelErrorTimer() {
+            if (undefined!=this.errorTimer) {
+                this.errorTimer = undefined;
+            }
+        },
+        i18n(str) {
+            if (this.show) {
+                return i18n(str);
+            } else {
+                return str;
+            }
+        }
+    },
+    watch: {
+        'show': function(val) {
+            bus.$emit('dialogOpen', 'podcastadd', val);
+        }
+    },
+    beforeDestroy() {
+        this.cancelErrorTimer();
+    }
+})
+

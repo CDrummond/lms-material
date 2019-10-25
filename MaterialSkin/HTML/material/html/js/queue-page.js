39cd7d85ca7d3fe5317b7586b271c655ecddf370
Craig Drummond <craig.p.drummond@gmail.com>
2019-10-25 19:40:44 +0100

    Fix missing shortcuts from some queue action tooltips.

--- MaterialSkin/HTML/material/html/js/queue-page.js
+++ MaterialSkin/HTML/material/html/js/queue-page.js
@@ -202,13 +202,13 @@ var lmsQueue = Vue.component("lms-queue", {
    <v-btn :title="trans.shuffleOff" flat icon v-else-if="desktop || wide>0" class="toolbar-button dimmed" @click="bus.$emit('playerCommand', ['playlist', 'shuffle', 1])"><v-icon>shuffle</v-icon></v-btn>
    <v-divider vertical v-if="desktop || wide>0"></v-divider>
    <template v-if="desktop || wide>1" v-for="(action, index) in settingsMenuActions">
-    <v-btn flat icon @click.stop="headerAction(action)" class="toolbar-button" :title="ACTIONS[action].title | tooltip(ACTIONS[action].shortcut,keyboardControl)" :id="'tbar'+index">
+    <v-btn flat icon @click.stop="headerAction(action)" class="toolbar-button" :title="ACTIONS[action].title | tooltip(ACTIONS[action].key,keyboardControl,true)" :id="'tbar'+index">
       <img v-if="ACTIONS[action].svg" class="svg-img" :src="ACTIONS[action].svg | svgIcon(darkUi)"></img>
       <v-icon v-else>{{ACTIONS[action].icon}}</v-icon>
     </v-btn>
    </template>
-   <v-btn :title="trans.save | tooltip(trans.saveShortcut,keyboardControl)" flat icon @click="save()" class="toolbar-button"><v-icon>save</v-icon></v-btn>
-   <v-btn :title="trans.clear | tooltip(trans.clearShortcut,keyboardControl)" flat icon @click="clear()" class="toolbar-button"><img class="svg-list-img" :src="'queue-clear' | svgIcon(darkUi)"></img></v-btn>
+   <v-btn :title="trans.save | tooltip(LMS_SAVE_QUEUE_KEYBOARD,keyboardControl)" flat icon @click="save()" class="toolbar-button"><v-icon>save</v-icon></v-btn>
+   <v-btn :title="trans.clear | tooltip(LMS_CLEAR_QUEUE_KEYBOARD,keyboardControl)" flat icon @click="clear()" class="toolbar-button"><img class="svg-list-img" :src="'queue-clear' | svgIcon(darkUi)"></img></v-btn>
   </v-layout>
  </div>
  <v-list class="lms-list-sub bgnd-cover" id="queue-list" v-bind:class="{'lms-list-sub3':threeLines}">
@@ -291,7 +291,7 @@ var lmsQueue = Vue.component("lms-queue", {
             listSize:0,
             duration: 0.0,
             playerStatus: { shuffle:0, repeat: 0 },
-            trans: { ok: undefined, cancel: undefined, save:undefined, saveShortcut:undefined, clear:undefined, clearShortcut:undefined,
+            trans: { ok: undefined, cancel: undefined, save:undefined, clear:undefined,
                      repeatAll:undefined, repeatOne:undefined, repeatOff:undefined, shuffleAll:undefined, shuffleAlbums:undefined,
                      shuffleOff:undefined, selectMultiple:undefined, remove:undefined, dstm:undefined },
             menu: { show:false, item: undefined, x:0, y:0, index:0},
@@ -509,8 +509,7 @@ var lmsQueue = Vue.component("lms-queue", {
     },
     methods: {
         initItems() {
-            this.trans= { ok:i18n('OK'), cancel: i18n('Cancel'), save:i18n("Save queue"), saveShortcut:i18n("Ctrl(⌘)+%1", LMS_SAVE_QUEUE_KEYBOARD),
-                          clear:i18n("Clear queue"), clearShortcut:i18n("Ctrl(⌘)+%1", LMS_CLEAR_QUEUE_KEYBOARD),
+            this.trans= { ok:i18n('OK'), cancel: i18n('Cancel'), save:i18n("Save queue"), clear:i18n("Clear queue"),
                           repeatAll:i18n("Repeat queue"), repeatOne:i18n("Repeat single track"), repeatOff:i18n("No repeat"),
                           shuffleAll:i18n("Shuffle tracks"), shuffleAlbums:i18n("Shuffle albums"), shuffleOff:i18n("No shuffle"),
                           selectMultiple:i18n("Select multiple items"), remove:PQ_REMOVE_ACTION.title, dstm:i18n("Don't Stop The Music")};
@@ -967,8 +966,8 @@ var lmsQueue = Vue.component("lms-queue", {
         svgIcon: function (name, dark) {
             return "/material/svg/"+name+"?c="+(dark ? LMS_DARK_SVG : LMS_LIGHT_SVG)+"&r="+LMS_MATERIAL_REVISION;
         },
-        tooltip: function (str, shortcut, showShortcut) {
-            return showShortcut ? str+SEPARATOR+shortcut : str;
+        tooltip: function (str, key, showShortcut) {
+            return showShortcut ? str+SEPARATOR+shortcutStr(key) : str;
         }
     },
     watch: {

63a6e58a4b1d104850edcc0f86bf38e196ffc7d6
Craig Drummond <craig.drummond@roke.co.uk>
2019-10-25 19:40:18 +0100

    Place 'Server settings' back into main menu Closes #180

--- MaterialSkin/HTML/material/html/js/ui-settings.js
+++ MaterialSkin/HTML/material/html/js/ui-settings.js
@@ -463,7 +463,7 @@ Vue.component('lms-ui-settings', {
                          shortcutStr(ACTIONS[ADD_FAV_FOLDER_ACTION].skey, true)+SEPARATOR+ACTIONS[ADD_FAV_FOLDER_ACTION].title,
                          shortcutStr(LMS_TRACK_INFO_KEYBOARD)+SEPARATOR+i18n("Show current track information")];
             if (this.desktop) {
-                list.push(shortcutStr(LMS_EXPAND_NP_KEYBOARD)+SEPARATOR+i18n("Expand now playing"));
+                list.push(shortcutStr(LMS_EXPAND_NP_KEYBOARD, true),+SEPARATOR+i18n("Expand now playing"));
             }
             list.push(shortcutStr(LMS_SAVE_QUEUE_KEYBOARD)+SEPARATOR+i18n("Save queue"));
             list.push(shortcutStr(LMS_CLEAR_QUEUE_KEYBOARD)+SEPARATOR+i18n("Clear queue"));
@@ -472,6 +472,7 @@ Vue.component('lms-ui-settings', {
             list.push(shortcutStr(ACTIONS[PQ_SCROLL_ACTION].key)+SEPARATOR+ACTIONS[PQ_SCROLL_ACTION].title);
             list.push(shortcutStr(LMS_SETTINGS_KEYBOARD)+SEPARATOR+TB_UI_SETTINGS.title);
             list.push(shortcutStr(LMS_PLAYER_SETTINGS_KEYBOARD)+SEPARATOR+TB_PLAYER_SETTINGS.title);
+            list.push(shortcutStr(LMS_SERVER_SETTINGS_KEYBOARD)+SEPARATOR+TB_SERVER_SETTINGS.title);
             list.push(shortcutStr(LMS_INFORMATION_KEYBOARD)+SEPARATOR+TB_INFO.title);
             list.push(shortcutStr(LMS_MANAGEPLAYERS_KEYBOARD)+SEPARATOR+TB_MANAGE_PLAYERS.title);
             list.push(shortcutStr(LMS_SYNC_KEYBOARD)+SEPARATOR+i18n("Synchronise"));

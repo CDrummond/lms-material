4530e029ca7cc01b3c079564ad8c947ede3acc2f
Craig Drummond <craig.p.drummond@gmail.com>
2020-03-14 06:59:48 +0000

    Map more LMS icons to material icons.

--- MaterialSkin/HTML/material/html/js/icon-mapping.js
+++ MaterialSkin/HTML/material/html/js/icon-mapping.js
@@ -28,7 +28,13 @@ const ENDSWITH_MAP = {
     "/toptracks.png":{"svg":"trophy"},
     "/account.png":{"icon":"person"},
     "/artists.png":{"svg":"artist"},
-    "/whatsnew.png":{"icon":"new_releases"}
+    "/whatsnew.png":{"icon":"new_releases"},
+    "/albums.png":{"icon:":"album"},
+    "/playlists.png":{"icon:":"list"},
+    "/musicfolder.png":{"icon:":"folder"},
+    "/newmusic.png":{"icon:":"new_releases"},
+    "/years.png":{"icon:":"date_range"},
+    "/genres.png":{"icon:":"label"}
 };
 
 const INDEXOF_MAP = {

020cdc94486bb6777074dc7b7e4f11da1d7ee1a7
Craig Drummond <craig.p.drummond@gmail.com>
2020-03-16 12:07:41 +0000

    Don't hide 'Interface' from server settings, just reduce options.

--- MaterialSkin/HTML/material/html/js/information.js
+++ MaterialSkin/HTML/material/html/js/information.js
@@ -28,7 +28,7 @@ Vue.component('lms-information-dialog', {
     <ul>
      <template v-for="(info, index) in server"><li>{{info.label}}: {{info.text}}</li></template>
     </ul>
-    <v-btn v-longpress="openSettings" v-if="unlockAll" flat><v-icon class="btn-icon">{{TB_SERVER_SETTINGS.icon}}</v-icon>{{TB_SERVER_SETTINGS.title}}</v-btn>
+    <v-btn @click="openSettings" v-if="unlockAll" flat><v-icon class="btn-icon">{{TB_SERVER_SETTINGS.icon}}</v-icon>{{TB_SERVER_SETTINGS.title}}</v-btn>
     <div class="dialog-padding"></div>
    </div>
 
@@ -317,8 +317,8 @@ Vue.component('lms-information-dialog', {
                 return str;
             }
         },
-        openSettings(longPress) {
-            bus.$emit('dlg.open', 'iframe', '/material/settings/server/basic.html', TB_SERVER_SETTINGS.title, longPress);
+        openSettings() {
+            bus.$emit('dlg.open', 'iframe', '/material/settings/server/basic.html', TB_SERVER_SETTINGS.title);
         }
     },
     beforeDestroy() {

099dff0c806dc9080f50b1b938e881db3fa1ad1d
Craig Drummond <craig.p.drummond@gmail.com>
2020-03-18 12:24:35 +0000

    Show volume overlay when use keyboard to change volume.

---
+++ MaterialSkin/HTML/material/html/js/volume-overlay.js
@@ -0,0 +1,61 @@
+/**
+ * LMS-Material
+ *
+ * Copyright (c) 2018-2020 Craig Drummond <craig.p.drummond@gmail.com>
+ * MIT license.
+ */
+'use strict';
+
+Vue.component('lms-volumeoverlay', {
+    template: `
+   <div v-show="show && volume>-1 && dvc" id="keyboardVolumeOverlay">{{volume}}%</div>
+`,
+    props: [],
+    data() {
+        return {
+            volume: -1,
+            dvc: true,
+            show: false
+        }
+    },
+    mounted() {
+        if (!IS_MOBILE) {
+            bindKey('up', 'alt');
+            bindKey('down', 'alt');
+            bus.$on('keyboard', function(key, modifier) {
+                if (!this.$store.state.player || this.$store.state.visibleMenus.size>0 || (this.$store.state.openDialogs.length>0 && this.$store.state.openDialogs[0]!='info-dialog'))  {
+                    return;
+                }
+                if ('alt'==modifier  && (key=='up' || key=='down')) {
+                    this.startTimeout();
+                }
+            }.bind(this));
+            bus.$on('playerStatus', function(playerStatus) {
+                this.dvc = playerStatus.dvc;
+                var vol = Math.abs(playerStatus.volume);
+                if (vol!=this.volume) {
+                    this.volume = vol;
+                }
+            }.bind(this));
+        }
+    },
+    methods: {
+        startTimeout() {
+            this.show=true;
+            this.stopTimeout();
+            this.hideTimer = setTimeout(function () {
+                this.show=false;
+                this.hideTimer = undefined;
+            }.bind(this), 1500);
+        },
+        stopTimeout() {
+            if (this.hideTimer) {
+                clearTimeout(this.hideTimer);
+                this.hideTimer = undefined;
+            }
+        }
+    },
+    beforeDestroy() {
+        this.stopTimeout();
+    }
+})

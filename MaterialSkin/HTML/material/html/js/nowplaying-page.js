60b1a2476089afc9b2793f5f7f8b15c091d60d65
Craig Drummond <craig.p.drummond@gmail.com>
2020-04-21 08:41:53 +0100

    If current item has a 'remote_title' element and no 'album' element, then allow title (or remote_title) and artist/composer to span up to 3 lines each in now-playing. Issue #232

--- MaterialSkin/HTML/material/html/js/nowplaying-page.js
+++ MaterialSkin/HTML/material/html/js/nowplaying-page.js
@@ -151,12 +151,12 @@ var lmsNowPlaying = Vue.component("lms-now-playing", {
    <div v-if="landscape" v-touch:start="touchStart" v-touch:end="touchEnd" v-touch:moving="touchMoving">
     <img v-if="!info.show" :key="coverUrl" v-lazy="coverUrl" class="np-image-landscape" v-bind:class="{'np-image-landscape-wide': landscape && wide>1}" @contextmenu="showMenu" @click="clickImage(event)"></img>
     <div class="np-details-landscape">
-     <div class="np-text-landscape np-title" v-bind:class="{'np-text-landscape-1': lowHeight}" v-if="playerStatus.current.title">{{title | limitStr}}</div>
+     <div class="np-text-landscape np-title" v-bind:class="{'np-text-landscape-1':lowHeight,'np-text-landscape-3':!lowHeight&&threeLines}" v-if="playerStatus.current.title">{{title | limitStr}}</div>
      <div class="np-text-landscape" v-else>&nbsp;</div>
-     <div class="np-text-landscape subtext" v-bind:class="{'np-text-landscape-1': lowHeight}" v-if="playerStatus.current.artistAndComposer">{{playerStatus.current.artistAndComposer | limitStr}}</div>
+     <div class="np-text-landscape subtext" v-bind:class="{'np-text-landscape-1':lowHeight,'np-text-landscape-3':!lowHeight&&threeLines}" v-if="playerStatus.current.artistAndComposer">{{playerStatus.current.artistAndComposer | limitStr}}</div>
      <div class="np-text-landscape" v-else>&nbsp;</div>
-     <div class="np-text-landscape subtext" v-bind:class="{'np-text-landscape-1': lowHeight}" v-if="playerStatus.current.album">{{playerStatus.current.album | limitStr}}</div>
-     <div class="np-text-landscape subtext" v-bind:class="{'np-text-landscape-1': lowHeight}" v-else-if="playerStatus.current.remote_title && playerStatus.current.remote_title!=playerStatus.current.title">{{playerStatus.current.remote_title | limitStr}}</div>
+     <div class="np-text-landscape subtext" v-bind:class="{'np-text-landscape-1':lowHeight,'np-text-landscape-3':!lowHeight&&threeLines}" v-if="playerStatus.current.album">{{playerStatus.current.album | limitStr}}</div>
+     <div class="np-text-landscape subtext" v-bind:class="{'np-text-landscape-1':lowHeight,'np-text-landscape-3':!lowHeight&&threeLines}" v-else-if="playerStatus.current.remote_title && playerStatus.current.remote_title!=playerStatus.current.title">{{playerStatus.current.remote_title | limitStr}}</div>
      <div class="np-text-landscape" v-else>&nbsp;</div>
      <div v-if="showRatings && playerStatus.current.duration>0 && undefined!=rating.value" class="np-text-landscape">
       <v-rating v-if="maxRating>5" v-model="rating.value" half-increments hover clearable @click.native="setRating"></v-rating>
@@ -216,11 +216,11 @@ var lmsNowPlaying = Vue.component("lms-now-playing", {
    </div>
    <div v-else v-touch:start="touchStart" v-touch:end="touchEnd" v-touch:moving="touchMoving">
     <div v-bind:style="{height: portraitPad+'px'}"></div>
-    <p class="np-text np-title ellipsis" v-if="playerStatus.current.title">{{title}}</p>
+    <p class="np-text np-title ellipsis" v-if="playerStatus.current.title" v-bind:class="{'ellipsis3':threeLines}">{{title}}</p>
     <p class="np-text" v-else>&nbsp;</p>
-    <p class="np-text subtext ellipsis" v-if="playerStatus.current.artistAndComposer">{{playerStatus.current.artistAndComposer}}</p>
+    <p class="np-text subtext ellipsis" v-if="playerStatus.current.artistAndComposer" v-bind:class="{'ellipsis3':threeLines}">{{playerStatus.current.artistAndComposer}}</p>
     <p class="np-text" v-else>&nbsp;</p>
-    <p class="np-text subtext ellipsis" v-if="playerStatus.current.album">{{playerStatus.current.album}}</p>
+    <p class="np-text subtext ellipsis" v-if="playerStatus.current.album" v-bind:class="{'ellipsis3':threeLines}">{{playerStatus.current.album}}</p>
     <p class="np-text subtext ellipsis3" v-else-if="playerStatus.current.remote_title && playerStatus.current.remote_title!=playerStatus.current.title">{{playerStatus.current.remote_title}}</p>
     <p class="np-text" v-else>&nbsp;</p>
     <img v-if="!info.show" :key="coverUrl" v-lazy="coverUrl" class="np-image" @contextmenu="showMenu" @click="clickImage(event)"></img>
@@ -312,7 +312,8 @@ var lmsNowPlaying = Vue.component("lms-now-playing", {
                  disablePrev:true,
                  disableNext:true,
                  dstm:false,
-                 infoZoom:10
+                 infoZoom:10,
+                 threeLines:false
                 };
     },
     mounted() {
@@ -410,6 +411,8 @@ var lmsNowPlaying = Vue.component("lms-now-playing", {
         bus.$on('playerStatus', function(playerStatus) {
             var playStateChanged = false;
             var trackChanged = false;
+
+            this.threeLines = playerStatus.current.remote_title && !playerStatus.current.album;
 
             // Have other items changed
             if (playerStatus.isplaying!=this.playerStatus.isplaying) {

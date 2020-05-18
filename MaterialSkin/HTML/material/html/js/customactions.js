33deacc8772f41a53e21bcb878d905270f534860
Craig Drummond <craig.p.drummond@gmail.com>
2020-05-18 12:33:05 +0100

    Use 'Cancel' and not 'No', to be consistent with elsewhere

--- MaterialSkin/HTML/material/html/js/customactions.js
+++ MaterialSkin/HTML/material/html/js/customactions.js
@@ -44,7 +44,7 @@ function getCustomActions(id, lockedActions) {
 
 function performCustomAction(action, player) {
     if (undefined!=action.prompt) {
-        bus.$confirm(action.prompt, {buttonTrueText: i18n('Yes'), buttonFalseText: i18n('No')}).then(res => {
+        bus.$confirm(action.prompt, {buttonTrueText: i18n('Yes'), buttonFalseText: i18n('Cancel')}).then(res => {
             if (res) {
                 doCustomAction(action, player);
             }

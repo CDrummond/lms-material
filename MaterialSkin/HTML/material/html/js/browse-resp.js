400b0b68daccd0c46e80533a885c6f66c0122590
Craig Drummond <craig.p.drummond@gmail.com>
2021-02-16 21:35:14 +0000

    When showing list of composers, conductors, or bands, check the numerical role_id as well as the text version so that the correct string is shown.

--- MaterialSkin/HTML/material/html/js/browse-resp.js
+++ MaterialSkin/HTML/material/html/js/browse-resp.js
@@ -529,11 +529,11 @@ function parseBrowseResp(data, parent, options, cacheKey, parentCommand, parentG
                     if (typeof data.params[1][i] === 'string' || data.params[1][i] instanceof String) {
                         var lower = data.params[1][i].toLowerCase();
                         if (lower.startsWith("role_id:")) {
-                            if (lower=="role_id:composer") {
+                            if (lower=="role_id:composer" || lower=="role_id:2") {
                                 isComposers = true;
-                            } else if (lower=="role_id:conductor") {
+                            } else if (lower=="role_id:conductor" || lower=="role_id:3") {
                                 isConductors = true;
-                            } else if (lower=="role_id:band") {
+                            } else if (lower=="role_id:band" || lower=="role_id:4") {
                                 isBands = true;
                             }
                             break;

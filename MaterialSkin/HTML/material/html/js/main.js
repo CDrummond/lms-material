a912180c774b4acaa95ffbbd8c662689eac67af0
Craig Drummond <craig.p.drummond@gmail.com>
2022-01-09 17:16:03 +0000

    Remove logging

--- MaterialSkin/HTML/material/html/js/main.js
+++ MaterialSkin/HTML/material/html/js/main.js
@@ -28,7 +28,6 @@ var app = new Vue({
         }
         if (IS_IOS || queryParams.addpad) {
             // Always add padding...
-            console.log("ADD");
             document.documentElement.style.setProperty('--bottom-nav-pad', '12px');
             document.documentElement.style.setProperty('--bottom-progress-adjust', '4px');
             document.documentElement.style.setProperty('--bottom-toolbar-pad', '4px');

586d0ed9baea690876346367a3d7e56a325be6a5
CDrummond <craig.p.drummond@gmail.com>
2022-05-14 09:08:23 +0100

    Don't auto-start searches in lists as could be in middle of typing something. Auto-start is OK for 'My Music' search as this can be incremental.

--- MaterialSkin/HTML/material/html/js/text-field.js
+++ MaterialSkin/HTML/material/html/js/text-field.js
@@ -10,7 +10,12 @@ Vue.component('text-field', {
     template: `
  <v-text-field :label="title" :autofocus="focus" v-model.lazy="term" single-line clearable class="lms-search" @input="textChanged($event)" v-on:keyup.enter="emitValue" :append-icon="IS_MOBILE && term && type=='search' ? 'search' : ''" @click:append="emitValue" @blur="stopDebounce"></v-text-field>
 `,
-    props: ['title', 'focus', 'type'],
+    props: {
+        title:String,
+        focus:Boolean,
+        type:String,
+        debounce:Number
+    },
     data() {
         return {
             term: ""
@@ -28,9 +33,11 @@ Vue.component('text-field', {
         },
         textChanged(event) {
             this.stopDebounce();
-            this.debounceTimer = setTimeout(function () {
-                this.emitValue();
-            }.bind(this), 1000);
+            if (undefined!=this.debounce && this.debounce>0) {
+                this.debounceTimer = setTimeout(function () {
+                    this.emitValue();
+                }.bind(this), this.debounce);
+            }
         },
         emitValue() {
             this.stopDebounce();

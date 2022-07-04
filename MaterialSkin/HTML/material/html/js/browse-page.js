d44c63b1729e2537070c56442f6156d665b2a9a1
CDrummond <craig.p.drummond@gmail.com>
2022-07-04 10:01:18 +0100

    Don't show header subtitles in search results, as this mainly repeats title text.

--- MaterialSkin/HTML/material/html/js/browse-page.js
+++ MaterialSkin/HTML/material/html/js/browse-page.js
@@ -165,7 +165,7 @@ var lmsBrowse = Vue.component("lms-browse", {
     <v-list-tile v-else-if="item.header" class="lms-list-item" v-bind:class="{'browse-header': item.header && item.header!=PLAIN_HEADER}" @click="click(item, index, $event)">
      <v-list-tile-content>
       <v-list-tile-title>{{item.title}}</v-list-tile-title>
-      <v-list-tile-sub-title v-if="item.subtitle">{{item.subtitle}}</v-list-tile-sub-title>
+      <v-list-tile-sub-title v-if="item.subtitle && !item.hidesub">{{item.subtitle}}</v-list-tile-sub-title>
      </v-list-tile-content>
      <v-list-tile-action class="browse-action" v-if="undefined!=item.stdItem || (item.menu && item.menu.length>0)" :title="i18n('%1 (Menu)', item.title)">
       <div v-if="hoverBtns && 0==selection.size && (undefined!=item.stdItem || (item.menu && (item.menu[0]==PLAY_ACTION || item.menu[0]==PLAY_ALL_ACTION)))" class="list-btns">

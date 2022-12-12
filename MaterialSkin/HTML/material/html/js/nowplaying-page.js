17383b532110aec1234ebd66da91e6d7f471e88b
CDrummond <craig.p.drummond@gmail.com>
2022-12-12 02:48:23 +0000

    Show Pandora thumbs up/down buttons where ratings would be in bottom bar of desktop layout.

--- MaterialSkin/HTML/material/html/js/nowplaying-page.js
+++ MaterialSkin/HTML/material/html/js/nowplaying-page.js
@@ -78,12 +78,13 @@ var lmsNowPlaying = Vue.component("lms-now-playing", {
      <v-list-tile-sub-title v-else-if="playerStatus.current.title">&#x22ef;</v-list-tile-sub-title>
     </v-list-tile-content>
     <v-list-tile-action>
-     <div v-if="playerStatus.playlist.count<2 || !(showRatings && !techInfo)" class="np-time-desktop link-item" v-bind:class="{'np-time-desktop-r': techInfo && showRatings}" @click="toggleTime()">{{formattedTime}}</div>
-     <div v-else class="np-time-desktop link-item" v-bind:class="{'np-time-desktop-r': techInfo && showRatings}" @click="toggleTime()">{{formattedTime}}{{playerStatus.playlist.current | trackCount(playerStatus.playlist.count, SEPARATOR)}}</div>
-     <div v-if="techInfo" class="np-tech-desktop ellipsis" v-bind:class="{'np-tech-desktop-r': showRatings}">{{technicalInfo}}</div>
-     <div v-else-if="playerStatus.playlist.count>1 && !showRatings" class="np-tech-desktop">{{playerStatus.playlist.current | trackCount(playerStatus.playlist.count)}}</div>
-     <div v-else-if="!showRatings" class="np-tech-desktop">&nbsp;</div>
-     <v-rating v-if="showRatings" class="np-rating-desktop" v-model="rating.value" half-increments hover clearable @click.native="setRating(true)" :readonly="undefined==ratingsPlugin"></v-rating>
+     <div v-if="playerStatus.playlist.count<2 || !(npBarRatings && !techInfo)" class="np-time-desktop link-item" v-bind:class="{'np-time-desktop-r': techInfo && npBarRatings}" @click="toggleTime()">{{formattedTime}}</div>
+     <div v-else class="np-time-desktop link-item" v-bind:class="{'np-time-desktop-r': techInfo && npBarRatings}" @click="toggleTime()">{{formattedTime}}{{playerStatus.playlist.current | trackCount(playerStatus.playlist.count, SEPARATOR)}}</div>
+     <div v-if="techInfo" class="np-tech-desktop ellipsis" v-bind:class="{'np-tech-desktop-r': npBarRatings}">{{technicalInfo}}</div>
+     <div v-else-if="playerStatus.playlist.count>1 && !npBarRatings" class="np-tech-desktop">{{playerStatus.playlist.current | trackCount(playerStatus.playlist.count)}}</div>
+     <div v-else-if="!npBarRatings" class="np-tech-desktop">&nbsp;</div>
+     <div v-if="npBarRatings && (repAltBtn.show || shuffAltBtn.show)" class="np-rating-desktop np-thumbs-desktop"><v-btn v-if="repAltBtn.show" :title="repAltBtn.tooltip" flat icon v-longpress="repeatClicked" v-bind:class="{'np-std-button': !stopButton,'disabled':noPlayer}"><v-icon v-if="repAltBtn.icon" class="media-icon">{{repAltBtn.icon}}</v-icon><img v-else :src="repAltBtn.image" class="btn-img"></img></v-btn><v-btn v-if="shuffAltBtn.show" :title="shuffAltBtn.tooltip" flat icon @click="shuffleClicked" v-bind:class="{'np-std-button': !stopButton}"><v-icon v-if="shuffAltBtn.icon" class="media-icon">{{shuffAltBtn.icon}}</v-icon><img v-else :src="shuffAltBtn.image" class="btn-img"></img></v-btn></div>
+     <v-rating v-else-if="showRatings" class="np-rating-desktop" v-model="rating.value" half-increments hover clearable @click.native="setRating(true)" :readonly="undefined==ratingsPlugin"></v-rating>
     </v-list-tile-action>
    </v-list-tile>
   </v-list>
@@ -1177,6 +1178,15 @@ var lmsNowPlaying = Vue.component("lms-now-playing", {
         },
         ratingsPlugin() {
             return this.$store.state.ratingsPlugin
+        },
+        npBarRatings() {
+            if (!this.playerStatus || !this.playerStatus.current) {
+                return false;
+            }
+            if (this.repAltBtn.show || this.shuffAltBtn.show) {
+                return true; // Use same space for these...
+            }
+            return this.showRatings;
         },
         showRatings() {
             return this.$store.state.showRating && this.playerStatus && this.playerStatus.current &&

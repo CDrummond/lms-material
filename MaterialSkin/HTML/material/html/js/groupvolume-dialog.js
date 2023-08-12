a7b4647a17ff2d61a2d3f797e0d77c6365f0d51b
CDrummond <craig.p.drummond@gmail.com>
2023-08-12 15:36:46 +0100

    Use bold font for average vol title?

--- MaterialSkin/HTML/material/html/js/groupvolume-dialog.js
+++ MaterialSkin/HTML/material/html/js/groupvolume-dialog.js
@@ -28,10 +28,10 @@ Vue.component('lms-groupvolume', {
 <v-sheet v-model="show" v-if="show" elevation="5" class="vol-sheet group-vol noselect">
  <v-container grid-list-md text-xs-center id="gv-container">
   <v-layout row wrap>
-   <div v-for="(player, index) in players" style="width:100%" :key="player.id" v-bind:class="{'active-player':currentPlayer && currentPlayer.id === player.id}" :id="currentPlayer && currentPlayer.id === player.id ? 'gv-active' : ('gv-'+index)">
+   <div v-for="(player, index) in players" style="width:100%" :key="player.id" v-bind:class="{'active-player':currentPlayer && currentPlayer.id === player.id,'group-vol-av':GRP_PLAYER_ID==player.id}" :id="currentPlayer && currentPlayer.id === player.id ? 'gv-active' : ('gv-'+index)">
     <v-flex :disabled="VOL_HIDDEN==player.dvc" xs12 style="height:8px"></v-flex>
     <volume-control :value="player.volume" :muted="player.muted" :playing="player.isplaying" :dvc="player.dvc" :layout="0" :name="player.name" :id="player.id" @inc="volumeUp" @dec="volumeDown" @changed="setVolume" @moving="movingSlider" @toggleMute="toggleMute"></volume-control>
-    <v-flex xs12 style="height:16px"></v-flex>
+    <v-flex xs12 style="height:8px"></v-flex>
    </div>
   </v-layout>
  </v-container>

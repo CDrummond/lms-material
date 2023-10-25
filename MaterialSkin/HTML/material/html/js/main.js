8e322bb027033dbcf07ccf95e2e9f852829a2e9d
CDrummond <craig.p.drummond@gmail.com>
2023-10-25 17:06:45 +0100

    Add missing bracket!

--- MaterialSkin/HTML/material/html/js/main.js
+++ MaterialSkin/HTML/material/html/js/main.js
@@ -167,7 +167,7 @@ var app = new Vue({
                     lmsOptions.allowDownload = false;
                     setLocalStorageVal('allowDownload', false);
                 }
-                if (undefined!=data.result['releaseTypeOrder'])
+                if (undefined!=data.result['releaseTypeOrder']) {
                     let arr = splitString(data.result['releaseTypeOrder'].split("\r").join("").split("\n").join(","));
                     lmsOptions.releaseTypeOrder = arr.lenght>0 ? arr : undefined;
                 }

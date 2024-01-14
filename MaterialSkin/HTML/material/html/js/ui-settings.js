e92a108464fb1b24f57df9e22f0bb41914e3b7cb
CDrummond <craig.p.drummond@gmail.com>
2024-01-14 09:23:33 +0000

    Consistent layout

--- MaterialSkin/HTML/material/html/js/ui-settings.js
+++ MaterialSkin/HTML/material/html/js/ui-settings.js
@@ -621,15 +621,17 @@ Vue.component('lms-ui-settings', {
                 { key:MBAR_THIN, label:i18n("Thin (single line of text)")},
                 { key:MBAR_THICK, label:i18n("Thick (two lines of text)")}
                 ];
-            this.skipSecondsOptions = [ { value: 5,  label: i18n("%1 seconds", 5) },
-                               { value: 10, label: i18n("%1 seconds", 10)},
-                               { value: 15, label: i18n("%1 seconds", 15)},
-                               { value: 30, label: i18n("%1 seconds", 30)}
-                             ];
-            this.fontSizes = [ { key: 's',  label: i18n("Small") },
-                               { key: 'r',  label: i18n("Regular") },
-                               { key: 'l',  label: i18n("Large") }
-                                ];
+            this.skipSecondsOptions = [
+                { value: 5,  label: i18n("%1 seconds", 5) },
+                { value: 10, label: i18n("%1 seconds", 10)},
+                { value: 15, label: i18n("%1 seconds", 15)},
+                { value: 30, label: i18n("%1 seconds", 30)}
+                ];
+            this.fontSizes = [
+                { key: 's',  label: i18n("Small") },
+                { key: 'r',  label: i18n("Regular") },
+                { key: 'l',  label: i18n("Large") }
+                ];
         },
         close() {
             this.show=false;

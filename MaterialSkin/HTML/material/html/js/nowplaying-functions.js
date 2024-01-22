65fc6f2eb3ef5d9759239e78f96d35507f4290c9
CDrummond <craig.p.drummond@gmail.com>
2024-01-22 12:07:27 +0000

    Fix album cover not showing when click on "Browse", "Go to album", or "+N more" links in now-playing/MAI.

--- MaterialSkin/HTML/material/html/js/nowplaying-functions.js
+++ MaterialSkin/HTML/material/html/js/nowplaying-functions.js
@@ -327,7 +327,7 @@ function nowplayingShowMenu(view, event) {
             view.menu.items.push({title:i18n("Go to band"), act:NP_BROWSE_CMD, cmd:{command:["albums"], params:["artist_id:"+view.playerStatus.current.band_id, ARTIST_ALBUM_TAGS, SORT_KEY+ARTIST_ALBUM_SORT_PLACEHOLDER, "role_id:BAND"], title:view.playerStatus.current.band}, svg:"trumpet"});
         }
         if (view.playerStatus.current.album_id && view.playerStatus.current.album) {
-            view.menu.items.push({title:ACTIONS[GOTO_ALBUM_ACTION].title, act:NP_BROWSE_CMD, cmd:{command:["tracks"], params:["album_id:"+view.playerStatus.current.album_id, trackTags(), SORT_KEY+"tracknum"], title:view.playerStatus.current.album}, icon:ACTIONS[GOTO_ALBUM_ACTION].icon});
+            view.menu.items.push({title:ACTIONS[GOTO_ALBUM_ACTION].title, act:NP_BROWSE_CMD, cmd:{command:["tracks"], params:["album_id:"+view.playerStatus.current.album_id, trackTags(true), SORT_KEY+"tracknum"], title:view.playerStatus.current.album}, icon:ACTIONS[GOTO_ALBUM_ACTION].icon});
         }
         let act = isInFavorites(view.playerStatus.current) ? REMOVE_FROM_FAV_ACTION : ADD_TO_FAV_ACTION;
         if (undefined!=view.playerStatus.current.favUrl && undefined!=view.playerStatus.current.favIcon) {
@@ -816,7 +816,7 @@ function nowplayingFetchAlbumInfo(view) {
             });
         }
         if (view.infoTrack.album_id!=undefined && view.infoTrack.album_id>=0) {
-            lmsList("", ["tracks"], ["album_id:"+view.infoTrack.album_id, trackTags()+(view.$store.state.showRating ? "R" : ""), "sort:tracknum"], 0, 1000, false, view.info.tabs[ALBUM_TAB].reqId).then(({data}) => {
+            lmsList("", ["tracks"], ["album_id:"+view.infoTrack.album_id, trackTags(true)+(view.$store.state.showRating ? "R" : ""), "sort:tracknum"], 0, 1000, false, view.info.tabs[ALBUM_TAB].reqId).then(({data}) => {
                 logJsonMessage("RESP", data);
                 if (data && data.result && view.isCurrent(data, ALBUM_TAB)) {
                     let resp = parseBrowseResp(data);
@@ -867,10 +867,10 @@ function nowplayingItemClicked(view, tab, section, index, event) {
 
 function nowplayingMoreClicked(view, tab, section) {
     if (ARTIST_TAB==tab && 0==section) {
-        bus.$emit("browse", ["albums"], ["artist_id:"+view.infoTrack.artist_id, ALBUM_TAGS, "sort:yearalbum"], unescape(view.infoTrack.artist), NP_INFO);
+        bus.$emit("browse", ["albums"], ["artist_id:"+view.infoTrack.artist_id, ALBUM_TAGS, SORT_KEY+"yearalbum"], unescape(view.infoTrack.artist), NP_INFO);
         view.info.show=false;
     } else if (ALBUM_TAB==tab && 0==section) {
-        bus.$emit("browse", ["tracks"], ["album_id:"+view.infoTrack.album_id, trackTags(), "sort:tracknum"], unescape(view.infoTrack.album), NP_INFO);
+        bus.$emit("browse", ["tracks"], ["album_id:"+view.infoTrack.album_id, trackTags(true), SORT_KEY+"tracknum"], unescape(view.infoTrack.album), NP_INFO);
         view.info.show=false;
     }
 }

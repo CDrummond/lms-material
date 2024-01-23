361fad1538ccf8918c1a86487d4333a9bd84565c
CDrummond <craig.p.drummond@gmail.com>
2024-01-23 17:17:54 +0000

    Add a button to server-side settings to reset genre lists to default values, translated into current language.

--- MaterialSkin/Settings.pm
+++ MaterialSkin/Settings.pm
@@ -11,21 +11,33 @@ package Plugins::MaterialSkin::Settings;
 use strict;
 use base qw(Slim::Web::Settings);
 use Slim::Utils::Prefs;
+use Slim::Utils::Strings qw(string cstring);
 
 my $prefs = preferences('plugin.material-skin');
 
 sub name {
-	return 'MATERIAL_SKIN';
+    return 'MATERIAL_SKIN';
 }
 
 sub page {
-	return 'plugins/MaterialSkin/settings/basic.html';
+    return 'plugins/MaterialSkin/settings/basic.html';
 }
 
 sub prefs {
-	return ($prefs, 'composergenres', 'conductorgenres', 'bandgenres', 'showComposer', 'showConductor', 'showBand', 'password', 'respectFixedVol',
-	        'showAllArtists', 'artistFirst', 'allowDownload', 'commentAsDiscTitle', 'showComment', 'pagedBatchSize', 'noArtistFilter',
-			'releaseTypeOrder', 'genreImages', 'touchLinks', 'yearInSub', 'playShuffle');
+    return ($prefs, 'composergenres', 'conductorgenres', 'bandgenres', 'showComposer', 'showConductor', 'showBand', 'password', 'respectFixedVol',
+            'showAllArtists', 'artistFirst', 'allowDownload', 'commentAsDiscTitle', 'showComment', 'pagedBatchSize', 'noArtistFilter',
+            'releaseTypeOrder', 'genreImages', 'touchLinks', 'yearInSub', 'playShuffle');
+}
+
+sub handler {
+    my ($class, $client, $params) = @_;
+
+    if ($params->{'load_def_genres'}) {
+		$params->{'pref_composergenres'} = string('PLUGIN_MATERIAL_SKIN_DEFAULT_COMPOSER_GENRES');
+        $params->{'pref_conductorgenres'} = string('PLUGIN_MATERIAL_SKIN_DEFAULT_CONDUCTOR_GENRES');
+        $params->{'pref_bandgenres'} = string('PLUGIN_MATERIAL_SKIN_DEFAULT_BAND_GENRES');
+    }
+    return $class->SUPER::handler($client, $params);
 }
 
 1;

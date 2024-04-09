d521953577dff6b5d04116ab310e6393fc95b2e4
CDrummond <craig.p.drummond@gmail.com>
2024-04-09 15:42:36 +0100

    Composer as title Work as subtitle

--- MaterialSkin/HTML/material/html/js/browse-resp.js
+++ MaterialSkin/HTML/material/html/js/browse-resp.js
@@ -1730,22 +1730,16 @@ function parseBrowseResp(data, parent, options, cacheKey, parentCommand, parentG
             resp.subtitle=0==resp.items.length ? i18n("Empty") : i18np("1 Item", "%1 Items", resp.items.length);
             resp.canUseGrid=true;
         } else if (data.result.works_loop) {
-            var isArtistWorks = undefined==parent || undefined==parent.id || !parent.id.startsWith(MUSIC_ID_PREFIX);
             for (var idx=0, loop=data.result.works_loop, loopLen=loop.length; idx<loopLen; ++idx) {
                 var i = loop[idx];
-                var key = isArtistWorks ? undefined : removeDiactrics(i.textkey);
+                var key = removeDiactrics(i.textkey);
                 if (undefined!=key && (resp.jumplist.length==0 || resp.jumplist[resp.jumplist.length-1].key!=key) && !textKeys.has(key)) {
                     resp.jumplist.push({key: key, index: resp.items.length});
                     textKeys.add(key);
                 }
                 resp.items.push({
-                    title: isArtistWorks ? i.work : i.composer,
-                    subtitle: isArtistWorks
-                         ? (undefined==parent || (parent.title!=i.composer && parent.subtitle!=i.composer))
-                             ? i.composer
-                             : undefined
-                         : i.work,
-                    composer: i.composer,
+                    title: i.composer,
+                    subtitle: i.work,
                     composer_id: i.composer_id,
                     id: "work_id:"+i.work_id,
                     type: "group",
@@ -1753,9 +1747,6 @@ function parseBrowseResp(data, parent, options, cacheKey, parentCommand, parentG
                     stdItem: STD_ITEM_WORK,
                     textkey: key
                 });
-            }
-            if (isArtistWorks) {
-                resp.items.sort(titleSort);
             }
             resp.subtitle=0==resp.items.length ? i18n("Empty") : i18np("1 Work", "%1 Works", resp.items.length);
             resp.canUseGrid=false;

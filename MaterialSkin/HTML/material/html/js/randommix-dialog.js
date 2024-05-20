20faa452f471604cceda4af42dfc8ef51d1c06d9
CDrummond <craig.p.drummond@gmail.com>
2024-05-20 12:07:25 +0100

    Get genre list for 'Random Mix' from 'randomplaygenrelist' command.

--- MaterialSkin/HTML/material/html/js/randommix-dialog.js
+++ MaterialSkin/HTML/material/html/js/randommix-dialog.js
@@ -100,63 +100,9 @@ Vue.component('lms-randommix', {
                     this.choseMix = "tracks";
                 }
 
-                lmsList(this.playerId, ["randomplaylibrarylist"], undefined, 0, 500).then(({data}) => {
-                    if (data && data.result && data.result.item_loop && data.result.item_loop.length>0) {
-                        this.libraries = [];
-                        this.library = undefined;
-                        for (var i=0, len=data.result.item_loop.length; i<len; ++i) {
-                            var id = data.result.item_loop[i].actions.do.cmd[1];
-                            if (undefined!=id && (""+id).length>2) {
-                                this.libraries.push({name:data.result.item_loop[i].text.replace(SIMPLE_LIB_VIEWS, ""), id:""+id});
-                                if (parseInt(data.result.item_loop[i].radio)==1) {
-                                    this.library = ""+id;
-                                }
-                            }
-                        }
-                        this.libraries.sort(nameSort);
-                        this.libraries.unshift({name: i18n("All"), id:LMS_DEFAULT_LIBRARY});
-                        if (undefined==this.library || LMS_DEFAULT_LIBRARIES.has(this.library)) {
-                            this.library = LMS_DEFAULT_LIBRARY;
-                        }
-                    }
-                });
-
-                lmsList(this.playerId, ["genres"], undefined, 0, 1000).then(({data}) => {
-                    this.chosenGenres = [];
-                    this.genres = [];
-                    // Get list of all genres (randomplaygenrelist filters on library chosen for mix!)
-                    if (data && data.result && data.result.genres_loop) {
-                        for (var idx=0, loop=data.result.genres_loop, loopLen=loop.length; idx<loopLen; ++idx) {
-                            this.genres.push(loop[idx].genre);
-                        }
-                    }
-                    lmsList(this.playerId, ["randomplaygenrelist"], undefined, 0, 500).then(({data}) => {
-                        if (data && data.result && data.result.item_loop) {
-                            data.result.item_loop.forEach(i => {
-                                if (i.checkbox==1) {
-                                    this.chosenGenres.push(i.text);
-                                }
-                            });
-                            this.show=true;
-                        }
-                    });
-                });
-
-                lmsCommand("", ["pref", "plugin.randomplay:continuous", "?"]).then(({data}) => {
-                    if (data && data.result && data.result._p2 != null) {
-                        this.continuous = 1 == parseInt(data.result._p2);
-                    }
-                });
-                lmsCommand("", ["pref", "plugin.randomplay:newtracks", "?"]).then(({data}) => {
-                    if (data && data.result && data.result._p2 != null) {
-                        this.newTracks = parseInt(data.result._p2);
-                    }
-                });
-                lmsCommand("", ["pref", "plugin.randomplay:oldtracks", "?"]).then(({data}) => {
-                   if (data && data.result && data.result._p2 != null) {
-                        this.oldTracks = parseInt(data.result._p2);
-                    }
-                });
+                this.initGenres();
+                this.initLibraries();
+                this.initConfig();
             });
         }.bind(this));
         bus.$on('noPlayers', function() {
@@ -173,6 +119,88 @@ Vue.component('lms-randommix', {
         }.bind(this));
     },
     methods: {
+        initGenres() {
+            this.chosenGenres = [];
+            this.genres = [];
+            // If player is set to a virtual library then set to default, read genre list, and then reset.
+            // Otherwise just read genre list
+            // NOTE: Not 100% sure if this is required, but does no harm?
+            lmsCommand(this.playerId, ["libraries", "getid"]).then(({data}) => {
+                let playerLibId = undefined==data.result.id ? LMS_DEFAULT_LIBRARY : (""+data.result.id);
+                if (!LMS_DEFAULT_LIBRARIES.has(playerLibId)) {
+                    lmsCommand(this.playerId, ["material-skin-client", "set-lib", "id:"+LMS_DEFAULT_LIBRARY]).then(({datax}) => {
+                        this.getGenreList(playerLibId);
+                    });
+                } else {
+                    this.getGenreList(undefined);
+                }
+            });
+        },
+        getGenreList(playerLibId) {
+            lmsList(this.playerId, ["randomplaygenrelist"], undefined, 0, 2500).then(({data}) => {
+                if (undefined!=playerLibId) {
+                    // Re-set playerId
+                    lmsCommand(this.playerId, ["material-skin-client", "set-lib", "id:"+playerLibId])
+                }
+                if (data && data.result && data.result.item_loop) {
+                    let used = new Set();
+                    for (let idx=0, loop=data.result.item_loop, loopLen=loop.length; idx<loopLen; ++idx) {
+                        let item = loop[idx];
+    
+                        if (undefined!=item.checkbox && item.actions && item.actions.on && item.actions.on.cmd) {
+                            let name = item.actions.on.cmd[1];
+                            if (!used.has(name)) {
+                                this.genres.push(name);
+                                used.add(name);
+                                if (item.checkbox==1) {
+                                    this.chosenGenres.push(name);
+                                }
+                            }
+                        }
+                    }
+                }
+                this.show=true;
+            });
+        },
+        initLibraries() {
+            lmsList(this.playerId, ["randomplaylibrarylist"], undefined, 0, 500).then(({data}) => {
+                if (data && data.result && data.result.item_loop && data.result.item_loop.length>0) {
+                    this.libraries = [];
+                    this.library = undefined;
+                    for (var i=0, len=data.result.item_loop.length; i<len; ++i) {
+                        var id = data.result.item_loop[i].actions.do.cmd[1];
+                        if (undefined!=id && (""+id).length>2) {
+                            this.libraries.push({name:data.result.item_loop[i].text.replace(SIMPLE_LIB_VIEWS, ""), id:""+id});
+                            if (parseInt(data.result.item_loop[i].radio)==1) {
+                                this.library = ""+id;
+                            }
+                        }
+                    }
+                    this.libraries.sort(nameSort);
+                    this.libraries.unshift({name: i18n("All"), id:LMS_DEFAULT_LIBRARY});
+                    if (undefined==this.library || LMS_DEFAULT_LIBRARIES.has(this.library)) {
+                        this.library = LMS_DEFAULT_LIBRARY;
+                    }
+                }
+            });
+        },
+        initConfig() {
+            lmsCommand("", ["pref", "plugin.randomplay:continuous", "?"]).then(({data}) => {
+                if (data && data.result && data.result._p2 != null) {
+                    this.continuous = 1 == parseInt(data.result._p2);
+                }
+            });
+            lmsCommand("", ["pref", "plugin.randomplay:newtracks", "?"]).then(({data}) => {
+                if (data && data.result && data.result._p2 != null) {
+                    this.newTracks = parseInt(data.result._p2);
+                }
+            });
+            lmsCommand("", ["pref", "plugin.randomplay:oldtracks", "?"]).then(({data}) => {
+               if (data && data.result && data.result._p2 != null) {
+                    this.oldTracks = parseInt(data.result._p2);
+                }
+            });
+        },
         close() {
             this.show=false;
             setLocalStorageVal("rndmix.showAll", this.showAll);

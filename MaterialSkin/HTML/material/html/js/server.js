b6e6305ed51ad676c09d6b4c4808ca08f5acd875
CDrummond <craig.p.drummond@gmail.com>
2024-10-09 16:23:24 +0100

    Setting is now 'include' not 'exclude'

--- MaterialSkin/HTML/material/html/js/server.js
+++ MaterialSkin/HTML/material/html/js/server.js
@@ -121,9 +121,9 @@ function parseUserDefinedRoles(data) {
         if (undefined!=data[key]['id'] && undefined!=data[key]['name']) {
             let id = parseInt(data[key]['id']);
             let lkey = key.toLocaleLowerCase();
-            let exclude = 1==parseInt(data[key]['exclude']);
-            lmsOptions.userDefinedRoles[id] = {role:key, lrole:lkey, text:data[key]['name'], exclude:exclude};
-            if (exclude) {
+            let include = 1==parseInt(data[key]['include']);
+            lmsOptions.userDefinedRoles[id] = {role:key, lrole:lkey, text:data[key]['name']};
+            if (!include) {
                 excluded.push(lkey);
             }
             ARTIST_TYPES.push(lkey);

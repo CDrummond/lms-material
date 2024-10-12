d12e02803443261517af3be37508cdc2b8f9fc1f
CDrummond <craig.p.drummond@gmail.com>
2024-10-12 07:43:37 +0100

    Use list choice for move over fav folder

--- MaterialSkin/HTML/material/html/js/browse-page.js
+++ MaterialSkin/HTML/material/html/js/browse-page.js
@@ -1934,10 +1934,17 @@ var lmsBrowse = Vue.component("lms-browse", {
                             var toId = this.items[to].id.startsWith("item_id:")
                                             ? this.items[to].id.replace("item_id:", "to_id:")
                                             : "to_id:"+this.items[to].params.item_id;
+console.log(fromId, toId);
                             if (this.items[to].isFavFolder) {
-                                confirm(i18n("Move '%1' into '%2' folder, or move to position?", this.items[this.dragIndex].title, this.items[to].title), i18n("Folder"), undefined, i18n("Position")).then(res => {
-                                    if (res>0) {
-                                        lmsCommand(this.playerId(), ["favorites", "move", fromId, toId+(1==res ? ".0" : "")]).then(({data}) => {
+                                let choices = [
+                                    {val:1, title:i18n("Move into '%1'", this.items[this.dragIndex].title), svg:"folder-favorite"},
+                                    {val:2, title:i18n("Move position"), icon:ACTIONS[SCROLL_TO_ACTION].icon, svg:ACTIONS[SCROLL_TO_ACTION].svg}
+                                ]
+                                choose(i18n("Move '%1'", this.items[this.dragIndex].title, this.items[to].title), choices).then(choice => {
+                                    console.log(choice);
+                                    if (undefined!=choice && choice.val>0) {
+                                        console.log(choice.val);
+                                        lmsCommand(this.playerId(), ["favorites", "move", fromId, toId+(1==choice.val ? ".0" : "")]).then(({data}) => {
                                             this.refreshList();
                                         });
                                     }

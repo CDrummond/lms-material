e095f5f6943e5606262f13c674fab195056e97d0
darrell-k <darrell@darrell.org.uk>
2025-02-02 21:15:48 +0000

    Tweaks for grouping and subtitles
    Signed-off-by: darrell-k <darrell@darrell.org.uk>

--- MaterialSkin/HTML/material/html/js/server.js
+++ MaterialSkin/HTML/material/html/js/server.js
@@ -597,9 +597,6 @@ var lmsServer = Vue.component('lms-server', {
                               };
             if (data.playlist_loop && data.playlist_loop.length>0) {
                 player.current = makeHtmlSafe(data.playlist_loop[0]);
-                if (undefined!=player.current.title) {
-                    player.current.title = trackTitle(player.current);
-                }
                 splitMultiples(player.current, true);
                 player.current.isClassical = undefined!=player.current.isClassical && 1==parseInt(player.current.isClassical);
                 player.current.time = undefined==data.time ? undefined : "stop"==data.mode ? 0 : parseFloat(data.time);

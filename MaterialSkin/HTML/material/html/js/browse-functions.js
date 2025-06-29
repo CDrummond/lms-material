5417e9717c1555baf363e989aa516ecb6e5834bc
CDrummond <craig.p.drummond@gmail.com>
2025-06-29 19:25:19 +0100

    Remove 'Add to playlist' for Bliss and MusicIP mixes, not working.

--- MaterialSkin/HTML/material/html/js/browse-functions.js
+++ MaterialSkin/HTML/material/html/js/browse-functions.js
@@ -599,9 +599,11 @@ function browseHandleListResponse(view, item, command, resp, prevPage, appendIte
         }
         if (resp.isMusicMix || (("albums"==command.command[0] && view.items.length>0 && command.params.find(elem => elem=="sort:random")))) {
             view.currentActions.push({action:RELOAD_ACTION, weight:-1});
+            /* Remove 'Add to playlist' as its not working
             if (resp.isMusicMix && !queryParams.party) {
                 view.currentActions.push({action:ADD_TO_PLAYLIST_ACTION, weight:50});
             }
+            */
         }
         if (canAddAlbumSort && view.command.command.length>0 && view.command.command[0]=="albums" && view.items.length>0) {
             for (var i=0, len=view.command.params.length; i<len && canAddAlbumSort; ++i) {

026dbacdb3ea94c6846718cb3409ad272c704ca0
CDrummond <craig.p.drummond@gmail.com>
2025-09-17 12:40:53 +0100

    Fix scan progress page when opening from 'Information' dialog.

--- MaterialSkin/HTML/material/html/js/iframe-dialog.js
+++ MaterialSkin/HTML/material/html/js/iframe-dialog.js
@@ -7,7 +7,7 @@
 'use strict';
 
 const SLOW_PAGES = new Set(['SETUP_PLUGINS']);
-const LMS_PAGES =  new Set(['server', 'player', 'extras', 'lms', 'help']);
+const LMS_PAGES =  new Set(['server', 'player', 'extras', 'lms']);
 const LMS_STD_SETTINGS_PAGES =  new Set(['server', 'player']);
 
 let useDefaultSkinForServerSettings = true;
@@ -806,11 +806,9 @@ Vue.component('lms-iframe-dialog', {
                                         ? "dlserver"
                                         : page.startsWith("plugins/") && (page.indexOf("?player=")>0 || page.indexOf("&player=")>0)
                                             ? "extras"
-                                            : page == '/material/html/docs/index.html'
+                                            : page == '/material/html/docs/index.html' || page.startsWith('/material/')
                                                 ? "lms" // tech info, or 'extra' entry
-                                                : page == '/material/html/material-skin/index.html'
-                                                    ? "help"
-                                                    : "other";
+                                                : "other";
             this.show = true;
             this.showMenu = false;
             this.choiceMenu = {show:false, x:0}

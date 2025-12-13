d486dc0d2ea90b6e85f67128a592cd91d156c0d9
CDrummond <craig.p.drummond@gmail.com>
2025-12-13 21:21:49 +0000

    Add subtitle.

--- MaterialSkin/HTML/material/html/js/ui-settings.js
+++ MaterialSkin/HTML/material/html/js/ui-settings.js
@@ -822,7 +822,7 @@ Vue.component('lms-ui-settings', {
                     {id:DETAILED_HOME_STD_PREFIX+"artists_recentlyplayed", title:i18n('Recently Played Artists'), checked:false, svg:"artistrecent"}
                 );
                 this.detailedHomeItems.push(
-                    {id:DETAILED_HOME_STD_PREFIX+"artists_playcount", title:i18n('Most Played Artists'), checked:false, svg:"artiststar"}
+                    {id:DETAILED_HOME_STD_PREFIX+"artists_playcount", title:i18n('Most Played Artists'), subtitle:this.i18n('Based upon total play count.'), checked:false, svg:"artiststar"}
                 );
                 this.detailedHomeItems.push(
                     {id:DETAILED_HOME_STD_PREFIX+"artists_popular", title:i18n('Popular Artists'), subtitle:this.i18n('Based upon play count for the past 6 months only.'), checked:false, svg:"artistpopular"}

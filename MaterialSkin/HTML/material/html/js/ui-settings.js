7e4c91cc1b38411fe7a2f654ea7d0e021fd85e05
CDrummond <craig.p.drummond@gmail.com>
2026-03-31 17:29:05 +0100

    Better control of dividers

--- MaterialSkin/HTML/material/html/js/ui-settings.js
+++ MaterialSkin/HTML/material/html/js/ui-settings.js
@@ -139,9 +139,8 @@ Vue.component('lms-ui-settings', {
      <v-select :items="homeButtonValues" :label="i18n('Show home button')" v-model="homeButton" item-text="label" item-value="key"></v-select>
     </v-list-tile>
     <v-list-tile-sub-title style="padding-bottom:16px">{{i18n("When navigating into lists, show a home button to quickly navigate to the main (home) screen. Otherwise you can click on the title to show a menu that contains 'Home'.")}}</v-list-tile-sub-title>
-    <v-divider></v-divider>
+    <v-divider v-if="advanced && (showMoveDialogs || hasPassword)"></v-divider>
 
-    <v-divider v-if="showMoveDialogs && advanced"></v-divider>
     <v-list-tile v-if="showMoveDialogs && advanced">
      <v-list-tile-content @click="moveDialogs = !moveDialogs" class="switch-label">
       <v-list-tile-title>{{i18n('Reposition dialogs')}}</v-list-tile-title>
@@ -149,8 +148,8 @@ Vue.component('lms-ui-settings', {
      </v-list-tile-content>
      <v-list-tile-action><m3-switch v-model="moveDialogs"></m3-switch></v-list-tile-action>
     </v-list-tile>
-
     <v-divider v-if="hasPassword && advanced"></v-divider>
+
     <v-list-tile v-if="hasPassword && advanced">
      <v-text-field :label="i18n('Settings password')" clearable autocorrect="off" v-model="password" class="lms-search" :append-icon="showPassword ? 'visibility_off' : 'visibility'" @click:append="() => (showPassword = !showPassword)" :type="showPassword ? 'text' : 'password'"></v-text-field>
     </v-list-tile>
@@ -323,7 +322,6 @@ Vue.component('lms-ui-settings', {
      </v-list-tile-content>
     <v-list-tile-action><m3-switch v-model="nowPlayingContext"></m3-switch></v-list-tile-action>
     </v-list-tile>
-    <v-divider></v-divider>
 
     <div class="dialog-padding"></div>
     <v-header class="dialog-section-header">{{i18n('Queue')}}</v-header>
@@ -371,7 +369,7 @@ Vue.component('lms-ui-settings', {
      </v-list-tile-content>
     <v-list-tile-action><m3-switch v-model="queueContext"></m3-switch></v-list-tile-action>
     </v-list-tile>
-    <v-divider></v-divider>
+    <v-divider v-if="advanced"></v-divider>
 
     <v-list-tile v-if="advanced">
      <v-list-tile-content @click="autoCloseQueue = !autoCloseQueue" class="switch-label">
@@ -379,7 +377,7 @@ Vue.component('lms-ui-settings', {
       <v-list-tile-sub-title>{{i18n("Automatically close queue, in desktop layout and not pinned, a few seconds after last interaction.")}}</v-list-tile-sub-title>
      </v-list-tile-content>
     <v-list-tile-action><m3-switch v-model="autoCloseQueue"></m3-switch></v-list-tile-action>
-    </v-list-tile v-if="advanced">
+    </v-list-tile>
 
     <div class="dialog-padding" v-if="LMS_P_MAI && advanced"></div>
     <v-header class="dialog-section-header" v-if="LMS_P_MAI && advanced">{{i18n('Track Information')}}</v-header>
@@ -427,7 +425,7 @@ Vue.component('lms-ui-settings', {
     </v-list-tile>
     <v-divider></v-divider>
 
-    <v-list-tile v-if="advanced">
+    <v-list-tile>
      <v-list-tile-content @click="ndSearch = !ndSearch" class="switch-label">
       <v-list-tile-title>{{i18n('Search')}}</v-list-tile-title>
       <v-list-tile-sub-title>{{i18n('Add search field.')}}</v-list-tile-sub-title>

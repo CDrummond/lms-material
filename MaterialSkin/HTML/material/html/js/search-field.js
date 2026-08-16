608a9931bc65f29cb0e82710d91212722c91d5bd
CDrummond <craig.p.drummond@gmail.com>
2026-08-16 11:30:02 +0100

    Fix build.

--- MaterialSkin/HTML/material/html/js/search-field.js
+++ MaterialSkin/HTML/material/html/js/search-field.js
@@ -80,7 +80,7 @@ function buildSearchResp(view) {
         if (results[i].resp.itemCustomActions && undefined!=filter) {
             resp.itemCustomActions[filter.substring(FILTER_PREFIX.length)] = results[i].resp.itemCustomActions;
         }
-        let list = useList ? items : [];
+        let list = useList ? resp.items : [];
         for (let idx=0, loop=results[i].resp.items; idx<numItems; ++idx) {
             let itm = loop[idx];
             itm.filter=filter;
@@ -229,7 +229,8 @@ Vue.component('lms-search-field', {
                     bus.$emit('showMessage', i18n('No results found'));
                 } else {
                     this.results.sort(function(a, b) { return a.command.cat<b.command.cat ? -1 : 1; });
-                    this.$emit('results', item, {command:[], params:[]}, { ...buildSearchResp(this), baseActions:[], canUseGrid: false, jumplist:[], allowHoverBtns:true}, this.prevPage);
+                    let resp = buildSearchResp(this);
+                    this.$emit('results', item, {command:[], params:[]}, { items:resp.items, itemCustomActions:resp.itemCustomActions, baseActions:[], canUseGrid: false, jumplist:[], allowHoverBtns:true}, this.prevPage);
                 }
                 this.commands=[];
                 this.results=[];
